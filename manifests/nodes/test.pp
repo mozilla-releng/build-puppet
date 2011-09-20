f34931239f190730039e330705448e75057696b0
Dustin J. Mitchell <dustin@mozilla.com>
2011-09-19 20:52:45 -0500

    modification to try to trigger an update

--- manifests/nodes/test.pp
+++ manifests/nodes/test.pp
@@ -1,7 +1,7 @@
 node "relabs-win.build.mtv1.mozilla.com" {
     notify {
         "hello":
-            message => "This is your windows box on puppet.";
+            message => "This is your windows box on puppet, take II";
     }
 }
 
