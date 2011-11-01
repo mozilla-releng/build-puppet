afd28095f7c269a248a4be2b40195a95252ca38d
Dustin J. Mitchell <dustin@mozilla.com>
2011-11-01 18:21:00 -0500

    don't use 'nodes/*', as it does not reload

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,4 +3,8 @@ import "settings.pp"
 import "secrets.pp"
 import "stages.pp"
 
-import "nodes/*"
+# do not use nodes/* here, as the server wil fail to reload when new files are added
+import "nodes/abstract.pp"
+import "nodes/buildservices.pp"
+import "nodes/slaves.pp"
+import "nodes/test.pp"
