eafa20e7abd4934cf2d090c3cb3083ea0d5c6cc2
Dustin J. Mitchell <dustin@mozilla.com>
2012-03-05 19:03:39 -0600

    add relabs07

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,6 +2,10 @@ node "relabs-buildbot-master.build.mtv1.mozilla.com" {
     include toplevel::server
 }
 
+node "relabs07.build.mtv1.mozilla.com" {
+    include toplevel::slave::build
+}
+
 node "relabs08.build.mtv1.mozilla.com" {
     include toplevel::slave::build
 }
