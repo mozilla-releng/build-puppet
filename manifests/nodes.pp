15f233e264570c7f85fb6254acc087da09c01b18
Dustin J. Mitchell <dustin@mozilla.com>
2012-03-07 14:15:06 -0600

    add test system

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,6 +2,10 @@ node "relabs-buildbot-master.build.mtv1.mozilla.com" {
     include toplevel::server
 }
 
+node "dl120g7-r4-4844.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
 node "relabs07.build.mtv1.mozilla.com" {
     include toplevel::slave::build
 }
