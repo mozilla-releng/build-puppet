b3cf0873e47e44a300dfa3d5ce0882e94a2ccab4
Amy Rich <arich@mozilla.com>
2012-03-09 11:36:11 -0800

    adding nodes

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,10 +1,172 @@
-node "relabs-buildbot-master.build.mtv1.mozilla.com" {
-    include toplevel::server
-}
-
-node "dl120g7-r4-4844.build.scl1.mozilla.com" {
+node "bld-centos6-hp-001.build.scl1.mozilla.com" {
     include toplevel::slave::build
 }
+
+node "bld-centos6-hp-002.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-003.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-004.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-005.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-006.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-007.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-008.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-009.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-010.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-011.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-012.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-013.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-014.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-015.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-016.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-017.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-018.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-019.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-020.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-021.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-022.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-023.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-024.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-025.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-026.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-027.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-028.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-029.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-030.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-031.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-032.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-033.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-034.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-035.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-036.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-037.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-038.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-039.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-040.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-041.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+node "bld-centos6-hp-042.build.scl1.mozilla.com" {
+    include toplevel::slave::build
+}
+
+
 
 node "relabs07.build.mtv1.mozilla.com" {
     include toplevel::slave::build
@@ -12,6 +174,10 @@ node "relabs07.build.mtv1.mozilla.com" {
 
 node "relabs08.build.mtv1.mozilla.com" {
     include toplevel::slave::build
+}
+
+node "relabs-buildbot-master.build.mtv1.mozilla.com" {
+    include toplevel::server
 }
 
 node "relabs-slave.build.mtv1.mozilla.com" {
