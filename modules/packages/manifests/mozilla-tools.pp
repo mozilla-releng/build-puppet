820851aab6b70ffdbfec33feab2cacf75f0c2a9e
John Ford <jhford@mozilla.com>
2012-03-14 16:48:12 -0700

    install mock_mozilla and twisted

--- modules/packages/manifests/mozilla-tools.pp
+++ modules/packages/manifests/mozilla-tools.pp
@@ -5,17 +5,42 @@ class packages::mozilla-tools {
 
     case $operatingsystem{
         CentOS: {
+            # These are mozilla custom tools
             package {
                 "mozilla-python27":
                     ensure => latest;
                 "mozilla-python26":
                     ensure => latest;
+                "mozilla-python27-setuptools":
+                    ensure => latest;
+                "mozilla-python26-setuptools":
+                    ensure => latest;
                 "mozilla-python27-mercurial":
                     ensure => latest;
                 "mozilla-python27-virtualenv":
                     ensure => latest;
+                "mozilla-python27-twisted":
+                    ensure => latest;
                 "mozilla-git":
                     ensure => latest;
+                "mock_mozilla":
+                    ensure => latest;
+            }
+            # These are upstream CentOS packages we want
+            package {
+                "mock": ensure => latest;
+                "createrepo": ensure => latest;
+
+            }
+
+            # The puppet group type can't do this it seems
+            exec {
+                "mock_mozilla-add":
+                    require => [Package["mock_mozilla"],User["$config::builder_username"]],
+                    command => "/usr/bin/gpasswd -a $config::builder_username mock_mozilla";
+                "mock-add":
+                    require => [Package["mock"],User["$config::builder_username"]],
+                    command => "/usr/bin/gpasswd -a $config::builder_username mock";
             }
         }
         default: {
