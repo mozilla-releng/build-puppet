046b6b207851fc09cb05a311b429145d7f5cd1d7
John Ford <jhford@mozilla.com>
2012-03-21 16:32:45 -0700

    fix typo

--- modules/toplevel/manifests/slave/build.pp
+++ modules/toplevel/manifests/slave/build.pp
@@ -7,7 +7,7 @@ class toplevel::slave::build inherits toplevel::slave {
     include tweaks::nofile
     include sudoers
     class { 'packages::environment':
-        path_additions => ['/tools/git/bin/', '/tools/python27/bin/', '/tools/python27-mercurial'],
+        path_additions => ['/tools/git/bin/', '/tools/python27/bin/', '/tools/python27-mercurial/bin'],
     }
     include packages::environment
 }
