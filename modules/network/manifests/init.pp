ff396cc740556b6134a9de12edc49feea89fc850
Dustin J. Mitchell <dustin@mozilla.com>
2012-03-23 22:45:20 -0700

    manage ifcfg-eth0, and specificall add PEERNTP=no

--- modules/network/manifests/init.pp
+++ modules/network/manifests/init.pp
@@ -2,6 +2,7 @@ class network {
     # ensure that the host uses its fqdn
     case $operatingsystem {
         CentOS: {
+            # always set the hostname to the fqdn
             file {
                 "/etc/sysconfig/network":
                     content => "NETWORKING=yes\nHOSTNAME=$fqdn\n",
@@ -12,6 +13,12 @@ class network {
                 "run-hostname-centos":
                     command => "/bin/hostname $fqdn",
                     refreshonly => true;
+            }
+
+            # and ensure we don't use peer NTP configuration (as that comes from puppet)
+            file {
+                "/etc/sysconfig/network-scripts/ifcfg-eth0":
+                    content => template("network/centos6-ifcfg-eth0.erb");
             }
         }
     }
