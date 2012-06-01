6cefe1d81c425957b7c03e3fbe613142eb125ff5
Chris AtLee <catlee@mozilla.com>
2012-06-01 13:54:02 -0400

    Set site-level file defaults to owner => root, group => root, mode => 0644. r=dustin

--- manifests/site.pp
+++ manifests/site.pp
@@ -5,3 +5,10 @@ $extlookup_precedence = ["local-config", "default-config", "secrets"]
 # basic top-level classes with basic settings
 import "stages.pp"
 import "nodes.pp"
+
+# Default to root:root 0644 ownership
+File {
+    owner => 0,
+    group => 0,
+    mode => "0644",
+}
