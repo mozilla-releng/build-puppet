a674b445ffcc3de295f1b8f620bc88f7e1fc1e0b
Chris AtLee <catlee@mozilla.com>
2012-06-05 10:59:24 -0400

    Bug 734532: Use releng distro/platform specific repos. r=dustin

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -1,6 +1,8 @@
 class packages::setup {
     include config
 
+    $majorver = regsubst($operatingsystemrelease, '^(\d+)\.?.*', '\1')
+
     file {
         # this will ensure that any "stray" yum repos will be deleted
         "/etc/yum.repos.d":
@@ -22,12 +24,11 @@ class packages::setup {
             url_path => "repos/yum/mirrors/centos/6/latest/updates/$hardwaremodel",
             gpg_key => "puppet:///modules/packages/A82BA4B7.txt",
             gpg_key_pkg => 'gpg-pubkey-a82ba4b7-4e2df47d';
-        "mozilla-centos6-x86_64":
-            url_path => "repos/yum/mozilla/CentOS/6/$hardwaremodel";
-        "releng-public":
-            url_path => "repos/yum/releng/public/noarch";
         "puppetlabs":
             url_path => "repos/yum/mirrors/puppetlabs/el/6/products/$hardwaremodel";
+        "releng-public-${operatingsystem}${majorver}-${hardwaremodel}":
+            url_path => "repos/yum/releng/public/$operatingsystem/$majorver/$hardwaremodel";
+        "releng-public-${operatingsystem}${majorver}-noarch":
+            url_path => "repos/yum/releng/public/$operatingsystem/$majorver/noarch";
     }
-
 }
