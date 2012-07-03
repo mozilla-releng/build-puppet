989833e6cfdbf554fcf796affef3844bba991912
Kim Moir <kmoir@mozilla.com>
2012-07-03 15:34:04 -0400

    Bug 765821 - Disable unnecessary service on CentOS on puppetagain r=Dustin

--- modules/disableservices/manifests/common.pp
+++ modules/disableservices/manifests/common.pp
@@ -1,18 +1,23 @@
 class disableservices::common {
-    # This class disables unnecessary services common to both server and slave
-
+# This class disables unnecessary services common to both server and slave
     case $operatingsystem {
-        CentOS: {
-            service { ['avahi-daemon', 'avahi-dnsconfd', 'bluetooth','sendmail']:
-                enable => false,
-                ensure => stopped,
+        CentOS : {
+            service {
+                ['acpid', 'anacron', 'apmd', 'atd', 'auditd', 'autofs',
+                'avahi-daemon', 'avahi-dnsconfd', 'bluetooth', 'cpuspeed',
+                'cups', 'cups-config-daemon', 'gpm', 'hidd', 'hplip', 'kudzu',
+                'mcstrans', 'mdmonitor', 'pcscd', 'restorecond', 'rpcgssd',
+                'rpcidmapd', 'sendmail', 'smartd', 'vncserver',
+                'yum-updatesd'] :
+                    ensure => stopped,
             }
         }
-        Darwin: {
-            service { ['com.apple.blued']:
-               enable => false,
-               ensure => stopped,
+        Darwin : {
+            service {
+                ['com.apple.blued'] :
+                    enable => false,
+                    ensure => stopped,
             }
-       }
+        }
     }
 }
