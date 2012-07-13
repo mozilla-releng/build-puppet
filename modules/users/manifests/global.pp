a5294f3993948534d97003b3098b88a1ff1ba546
Kim Moir <kmoir@mozilla.com>
2012-07-13 08:12:15 -0400

    Bug 773455 - global.pp should exclude check for /etc/profile.d/releng-path.sh unless CentOS r=Dustin

--- modules/users/manifests/global.pp
+++ modules/users/manifests/global.pp
@@ -1,10 +1,14 @@
 class users::global {
     # add some custom stuff to $PATH; some of these may not exist everywhere,
     # but this doesn't hurt.
-    file {
-        # see bug 738040 - this can be removed eventually
-        "/etc/profile.d/releng-path.sh":
-            ensure => absent;
+    case $operatingsystem {
+        CentOS : {
+            file {
+            # see bug 738040 - this can be removed eventually
+                "/etc/profile.d/releng-path.sh" :
+                    ensure => absent ;
+            }
+        }
     }
 
     # put some basic information in /etc/motd
