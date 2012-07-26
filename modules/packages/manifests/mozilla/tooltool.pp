176f080a046e46156dd3a6fa9e11e3c853a8c73d
Kim Moir <kmoir@mozilla.com>
2012-07-26 16:47:13 -0400

    Bug 764948 - add darwin functionality to puppetagain modules (fix tooltool package wrt users::root) r=Dustin

--- modules/packages/manifests/mozilla/tooltool.pp
+++ modules/packages/manifests/mozilla/tooltool.pp
@@ -1,12 +1,13 @@
 class packages::mozilla::tooltool {
     include packages::mozilla::python27
+    include users::root
     # this package is simple enough that its source code is embedded in the
     # puppet repo.  It gets the Python intepreter added to its shebang line
     $python = '/tools/python27/bin/python2.7'
     file {
         "/tools/tooltool.py":
-            owner => root,
-            group => root,
+            owner => $users::root::username,
+            group => $users::root::group,
             mode => 0755,
             content => template("packages/tooltool.py");
     }
