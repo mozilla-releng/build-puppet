eff47f7de2610c3d84c48071804287e0776039b7
Dustin J. Mitchell <dustin@mozilla.com>
2012-07-30 16:14:40 -0700

    Bug 778942: set resolution, but not when puppetizing; r=kmoir

---
+++ modules/screenresolution/manifests/talos.pp
@@ -0,0 +1,27 @@
+class screenresolution::talos {
+    $width = 1280
+    $height = 1024
+    $depth = 32
+    $refresh = 60
+
+    case $::operatingsystem {
+        Darwin: {
+            include packages::mozilla::screenresolution 
+            $resolution = "${width}x${height}x${depth}@${refresh}"
+
+            # this can't run while puppetizing, since the automatic login isn't in place yet, and
+            # the login window does not allow screenresolution to run.
+            if (!$puppetizing) {
+                exec {
+                    "set-resolution":
+                        command => "/usr/local/bin/screenresolution set $resolution",
+                        unless => "/usr/local/bin/screenresolution get 2>&1 | /usr/bin/grep 'Display 0: $resolution'",
+                        require => Class["packages::mozilla::screenresolution"];
+                }
+            }
+        }
+        default: {
+            fail("Don't have a mechanism to set screen resolution on $::operatingsystem")
+        }
+    }
+}
--- modules/toplevel/manifests/slave/test.pp
+++ modules/toplevel/manifests/slave/test.pp
@@ -2,6 +2,7 @@ class toplevel::slave::test inherits toplevel::slave {
     # test slaves need a GUI, and to be logged in.  For now they're just Darwin,
     # so we get the GUI for free and just need to ensure VNC is enabled.
     include vnc
+    include screenresolution::talos
     include users::builder::autologin
 
     include ntp::atboot
