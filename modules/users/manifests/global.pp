bff199751edb4cb304f9de0cc6b6690da5d8a9fe
Dustin J. Mitchell <dustin@mozilla.com>
2012-08-06 07:25:17 -0700

    Bug 738040: remove ensure => absent; r=Callek

--- modules/users/manifests/global.pp
+++ modules/users/manifests/global.pp
@@ -1,16 +1,4 @@
 class users::global {
-    # add some custom stuff to $PATH; some of these may not exist everywhere,
-    # but this doesn't hurt.
-    case $operatingsystem {
-        CentOS : {
-            file {
-            # see bug 738040 - this can be removed eventually
-                "/etc/profile.d/releng-path.sh" :
-                    ensure => absent ;
-            }
-        }
-    }
-
     # put some basic information in /etc/motd
     motd {
         "hostid":
