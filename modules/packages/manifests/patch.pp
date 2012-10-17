eb58ef7e31d2ae0e8c77997bc4122108351ecf67
Kim Moir <kmoir@mozilla.com>
2012-10-17 08:47:45 -0400

    Bug 802249 - foopies should have patch installed by puppet

---
+++ modules/packages/manifests/patch.pp
@@ -0,0 +1,16 @@
+class packages::patch {
+    case $operatingsystem {
+        CentOS: {
+            package {
+                "patch":
+                    ensure => latest;
+            }
+        }
+         Darwin: {
+            #patch is installed with base image
+        }
+        default: {
+            fail("cannot install on $operatingsystem")
+        }
+    }
+}
