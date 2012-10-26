81c1e0c3b6ffa9bcc3a27f4084dd7ce2aeb40ef3
Dustin J. Mitchell <dustin@mozilla.com>
2012-10-26 12:31:33 -0400

    add mobile-imaging servers node definition; a=nodechange

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -54,6 +54,11 @@ node /puppetmaster-\d+\..*\.aws-.*\.mozilla\.com/ {
 }
 
 node "mobile-services.build.scl1.mozilla.com" {
-     $is_bmm_admin_host = 1
+     $is_bmm_admin_host = 0
+     include toplevel::server::bmm
+}
+
+node /mobile-imaging-\d+\.p\d+\.releng\.scl1\.mozilla\.com/ {
+     $is_bmm_admin_host = $fqdn ? { /^mobile-imaging-001/ => 1, default => 0 }
      include toplevel::server::bmm
 }
