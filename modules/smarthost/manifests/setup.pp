a97e3c6a0fd5b1ea463daf626c22331b6dc58449
Dustin J. Mitchell <dustin@mozilla.com>
2012-12-20 13:21:31 -0800

    Bug 823686: add /etc/aliases with root pointing to releng-shared; r=kmoir, r=rail

--- modules/smarthost/manifests/setup.pp
+++ modules/smarthost/manifests/setup.pp
@@ -1,13 +1,24 @@
 class smarthost::setup {
-   include ::config
+    include ::config
 
-   file { "/etc/postfix/main.cf":
-        owner => "postfix",
-        group => "postfix",
-        mode => 0644,
-        ensure => present,
-        content => template("smarthost/main.cf.erb"),
-        require => Class["smarthost::install"],
-        notify => Class["smarthost::daemon"],
-   }
+    file {
+        "/etc/postfix/main.cf":
+            owner => "postfix",
+            group => "postfix",
+            mode => 0644,
+            ensure => present,
+            content => template("smarthost/main.cf.erb"),
+            require => Class["smarthost::install"],
+            notify => Class["smarthost::daemon"];
+        "/etc/aliases":
+            source => "puppet:///modules/smarthost/aliases",
+            require => Class['smarthost::install'],
+            notify => Exec['newaliases'];
+    }
+
+    exec {
+        'newaliases':
+            command => "/usr/bin/newaliases",
+            refreshonly => true;
+    }
 }
