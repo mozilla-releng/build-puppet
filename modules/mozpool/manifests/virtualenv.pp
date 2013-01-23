bacf11469ae1608dce345ad86ddc3550197bce98
Dustin J. Mitchell <dustin@mozilla.com>
2013-01-23 15:27:27 -0500

    Bug 828492: add mozprocess prereq; r=Callek (irc)

--- modules/mozpool/manifests/virtualenv.pp
+++ modules/mozpool/manifests/virtualenv.pp
@@ -23,6 +23,7 @@ class mozpool::virtualenv {
                 "flup==1.0.3.dev-20110405",
                 "pymysql==0.5",
                 "mozdevice==0.9",
+                "mozprocess==0.8",
                 "mozpool==${mozpool::settings::mozpool_version}",
             ],
             notify => Service['supervisord'];
