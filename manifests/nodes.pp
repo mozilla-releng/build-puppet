ca296f1f8edeade05b75b3e4eb2f35670ed3cce4
Dustin J. Mitchell <dustin@mozilla.com>
2013-02-04 08:41:07 -0500

    Bug: none; add relabs03..05 and sort nodes by type; a=nodechange

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,11 +1,19 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-node /bld-centos6-hp-\d+.build.scl1.mozilla.com/ {
-    include toplevel::slave::build::mock
-}
+
+## relabs machines - check with dustin to borrow one
 
 node "relabs02.build.mtv1.mozilla.com" {
+}
+
+node "relabs03.build.mtv1.mozilla.com" {
+}
+
+node "relabs04.build.mtv1.mozilla.com" {
+}
+
+node "relabs05.build.mtv1.mozilla.com" {
 }
 
 node "relabs06.build.mtv1.mozilla.com" {
@@ -16,6 +24,8 @@ node "relabs07.build.mtv1.mozilla.com" {
 
 node "relabs08.build.mtv1.mozilla.com" {
 }
+
+## foopies
 
 node /foopy\d+.build.mtv1.mozilla.com/ {
     include toplevel::server::foopy
@@ -29,11 +39,30 @@ node /foopy\d+.p\d+.releng.scl1.mozilla.com/ {
     include toplevel::server::foopy
 }
 
+## testers
+
 node /talos-mtnlion-r5-\d+.test.releng.scl3.mozilla.com/ {
     include toplevel::slave::test
 }
 
+node /tst-.*\.build\.aws-.*\.mozilla\.com/ {
+    # Make sure we get our /etc/hosts set up
+    class {
+        "network::aws": stage => network,
+    }
+    include toplevel::slave::test
+}
+
+node "ix-mn-linux64-001.test.releng.scl3.mozilla.com" {
+}
+
+## builders
+
 node /linux64-ix-slave\d+.build.scl1.mozilla.com/ {
+    include toplevel::slave::build::mock
+}
+
+node /bld-centos6-hp-\d+.build.scl1.mozilla.com/ {
     include toplevel::slave::build::mock
 }
 
@@ -45,9 +74,13 @@ node /(bld|try|dev)-.*\.build\.aws-.*\.mozilla\.com/ {
     include toplevel::slave::build::mock
 }
 
+## puppetmasters
+
 node /puppetmaster-\d+\..*\.aws-.*\.mozilla\.com/ {
     include toplevel::server::puppetmaster::standalone
 }
+
+## mozpool servers
 
 node "mobile-imaging-stage1.p127.releng.scl1.mozilla.com" {
     $extra_root_keys = [ 'mcote' ]
@@ -63,13 +96,3 @@ node /mobile-imaging-\d+\.p\d+\.releng\.scl1\.mozilla\.com/ {
     include toplevel::server::mozpool
 }
 
-node /tst-.*\.build\.aws-.*\.mozilla\.com/ {
-    # Make sure we get our /etc/hosts set up
-    class {
-        "network::aws": stage => network,
-    }
-    include toplevel::slave::test
-}
-
-node "ix-mn-linux64-001.test.releng.scl3.mozilla.com" {
-}
