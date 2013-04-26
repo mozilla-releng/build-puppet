a895070873386a2772979bae00a99a256bfebb8b
Dustin J. Mitchell <dustin@mozilla.com>
2013-04-26 15:15:03 -0400

    Bug 825056: revert upgrade to 3.2.0-0.1rc1
    https://projects.puppetlabs.com/issues/20439

--- modules/packages/manifests/puppet.pp
+++ modules/packages/manifests/puppet.pp
@@ -7,7 +7,7 @@ class packages::puppet {
         'packages::puppet::end': ;
     }
 
-    $puppet_version = "3.2.0-0.1rc1.el6"
+    $puppet_version = "3.1.1-20130329git7541bae.1.el6"
     $puppet_rpm_version = "${puppet_version}" #-1.el6"
     $puppet_deb_version = "${puppet_version}-1mozilla1"
     $facter_version = "1.6.11"
