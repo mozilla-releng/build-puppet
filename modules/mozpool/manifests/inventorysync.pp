eb05aa2608e0f0936ad954b2ea3d7426bf0bac7d
Dustin J. Mitchell <dustin@mozilla.com>
2013-05-09 12:06:03 -0400

    condition mozpool config on staging aspect, not deprecated variable; a=bustage

--- modules/mozpool/manifests/inventorysync.pp
+++ modules/mozpool/manifests/inventorysync.pp
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class mozpool::inventorysync {
     # only the admin node should do the inventory sync, and only in production
-    if ($is_bmm_admin_host and !$mozpool_staging) {
+    if ($is_bmm_admin_host and !has_aspect('staging')) {
         file {
             "/etc/cron.d/mozpool-inventorysync":
                 content => "15,45 * * * * apache MOZPOOL_CONFIG=${::mozpool::settings::config_ini} ${::mozpool::settings::root}/frontend/bin/mozpool-inventorysync\n";
