f216fefe65f583a47a6045a0fcff062352e6e591
Ben Hearsum <bhearsum@mozilla.com>
2013-05-16 12:44:05 -0400

    Adjust bors master ref.

--- modules/bors/manifests/instance.pp
+++ modules/bors/manifests/instance.pp
@@ -1,6 +1,6 @@
 define bors::instance($basedir, $owner, $group, $repo_owner, $repo, $reviewers,
                       $builders, $buildbot_url, $bors_user, $bors_pass,
-                      $test_ref = "auto", $master_ref = "incoming",
+                      $test_ref = "auto", $master_ref = "master",
                       $n_builds = "5", $bors_version = "1.0") {
     bors::install {
         $title:
