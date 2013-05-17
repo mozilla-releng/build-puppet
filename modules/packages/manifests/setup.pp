66f9f89de51083683a51ec247af5927f3a2c82fe
Ben Hearsum <bhearsum@mozilla.com>
2013-05-17 14:15:49 -0400

    bug 873040: create newer freetype package for servo builds. r=dustin

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -58,7 +58,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 5
+            $repoflag = 6
             file {
                 "/etc/.repo-flag":
                     content =>
