486333f1a9a0c0ce70f6e6161956a0cef8a932bf
Dustin J. Mitchell <dustin@mozilla.com>
2013-05-23 16:39:26 -0400

    Bug 825056: add remaining two masters in each AZ; a=configchange

--- manifests/moco-config.pp
+++ manifests/moco-config.pp
@@ -24,13 +24,13 @@ class config inherits config::base {
         ],
         ".*\\.releng\\.(use1|aws-us-east-1)\\.mozilla\\.com" => [
             "releng-puppet1.srv.releng.use1.mozilla.com",
-            #"releng-puppet2.srv.releng.use1.mozilla.com",
-            #"releng-puppet3.srv.releng.use1.mozilla.com",
+            "releng-puppet2.srv.releng.use1.mozilla.com",
+            "releng-puppet3.srv.releng.use1.mozilla.com",
         ],
         ".*\\.releng\\.(usw2|aws-us-west-2)\\.mozilla\\.com" => [
             "releng-puppet1.srv.releng.usw2.mozilla.com",
-            #"releng-puppet2.srv.releng.usw2.mozilla.com",
-            #"releng-puppet3.srv.releng.usw2.mozilla.com",
+            "releng-puppet2.srv.releng.usw2.mozilla.com",
+            "releng-puppet3.srv.releng.usw2.mozilla.com",
         ],
     }
     $puppet_servers = sort_servers_by_group($grouped_puppet_servers)
