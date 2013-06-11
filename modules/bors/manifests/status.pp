df347e436177766c5bbd03e3d838b9146312a277
Ben Hearsum <bhearsum@mozilla.com>
2013-06-11 11:07:59 -0400

    bug 881702: pull request links are pointing to rust instead of servo. r=dustin
    --HG--
    rename : modules/bors/files/bors-render.js => modules/bors/templates/bors-render.js.erb

--- modules/bors/manifests/status.pp
+++ modules/bors/manifests/status.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define bors::status($owner, $group, $status_location) {
+define bors::status($owner, $group, $repo_owner, $repo, $status_location) {
     file {
         "${status_location}":
             owner => $owner,
@@ -23,7 +23,7 @@ define bors::status($owner, $group, $status_location) {
             owner => $owner,
             group => $group,
             mode => 644,
-            source => "puppet:///modules/bors/bors-render.js";
+            content => template("bors/bors-render.js.erb");
         "${status_location}/dom-util.js":
             owner => $owner,
             group => $group,
