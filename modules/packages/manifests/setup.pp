1540b9588cfaf7d77c2040ded83999d85eaefba9
Dustin J. Mitchell <dustin@mozilla.com>
2013-06-19 21:44:58 -0400

    no bug: clean yum caches; a=bustage

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -58,7 +58,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 7
+            $repoflag = 8
             file {
                 "/etc/.repo-flag":
                     content =>
