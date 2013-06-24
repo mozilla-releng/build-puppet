7be31568f392915ec1ebab8b3088368495893861
Ben Hearsum <bhearsum@mozilla.com>
2013-06-24 17:26:13 -0400

    Update Bors' Buildbot URL to use public interface. r=org-only

--- manifests/servo-config.pp
+++ manifests/servo-config.pp
@@ -138,6 +138,6 @@ class config inherits config::base {
     $bors_servo_repo = "servo"
     $bors_servo_reviewers = ["brson", "pcwalton", "metajack", "ILyoan", "jdm", "yichoi", "aydinkim", "sfowler"]
     $bors_servo_builders = ["linux", "mac"]
-    $bors_servo_buildbot_url = "http://buildbot-master-servo-01.srv.servo.releng.use1.mozilla.com:8001"
+    $bors_servo_buildbot_url = "http://servo-buildbot.pub.build.mozilla.org"
 }
 
