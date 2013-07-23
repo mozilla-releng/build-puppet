e8f8b5b0c2a9ab5ddbb8bad8b0be54952cb81cf4
Dustin J. Mitchell <dustin@mozilla.com>
2013-07-23 13:37:08 -0400

    Bug 894201: run NTP everywhere; r=rail

--- modules/ntp/manifests/daemon.pp
+++ modules/ntp/manifests/daemon.pp
@@ -2,61 +2,45 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class ntp::daemon {
+    include ::shared
+    include packages::ntp
+    include config
+    include users::root
 
-# NTP daemons don't work on VMWare, so don't install them there
-    if ($::virtual != "vmware") {
-        include ::shared
-        include packages::ntp
-        include config
-        include users::root
-
-        $ntpserver = $config::ntp_server
-        case $::operatingsystem {
-            CentOS, Ubuntu: {
-                file {
-                    "/etc/ntp.conf" :
-                        content => template("ntp/ntp.conf.erb"),
-                        mode => 644,
-                        owner => root,
-                        group => $users::root::group;
-                }
-                service {
-                    "ntpd" :
-                        subscribe => File["/etc/ntp.conf"],
-                        enable => true,
-                        hasstatus => true,
-                        ensure => running;
-                }
+    $ntpserver = $config::ntp_server
+    case $::operatingsystem {
+        CentOS, Ubuntu: {
+            file {
+                "/etc/ntp.conf" :
+                    content => template("ntp/ntp.conf.erb"),
+                    mode => 644,
+                    owner => root,
+                    group => $users::root::group;
             }
-            Darwin: {
-                service {
-                    "ntpdate" :
-                        enable => true,
-                        hasstatus => true;
-                }
-                exec {
-                    "set-time-server" :
-                    command => "/usr/sbin/systemsetup -setnetworktimeserver ${ntpserver}",
-                    refreshonly => true;
-                }
-                file {
-                    "$vardir/.puppet-ntpserver" :
-                        content => "$ntpserver",
-                        notify => Exec["set-time-server"];
-                }
+            service {
+                "ntpd" :
+                    subscribe => File["/etc/ntp.conf"],
+                    enable => true,
+                    hasstatus => true,
+                    ensure => running;
             }
         }
-    }
-    else {
-        # actively disable ntp on virtual machines, just in case
-        service {
-            "ntpd" :
-            enable => false,
-            hasstatus => true,
-            ensure => stopped ;
-            "ntpdate" :
-                enable => false,
-                hasstatus => false ;
+        Darwin: {
+            service {
+                "ntpdate" :
+                    enable => true,
+                    hasstatus => true;
+            }
+            exec {
+                "set-time-server" :
+                command => "/usr/sbin/systemsetup -setnetworktimeserver ${ntpserver}",
+                refreshonly => true;
+            }
+            file {
+                "$vardir/.puppet-ntpserver" :
+                    content => $ntpserver,
+                    notify => Exec["set-time-server"];
+            }
         }
     }
 }
