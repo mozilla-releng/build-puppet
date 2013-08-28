315b1fe801ebd50821b6b64bc07d149dbac8ebb5
Justin Wood <Callek@gmail.com>
2013-08-28 18:55:35 -0400

    Bug 895186 - Add android-sdk to the Ubuntu systems. r=rail For: Run Android x86 emulator unit tests from buildbot.

---
+++ modules/packages/manifests/mozilla/android_sdk18.pp
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class packages::mozilla::android_sdk18 {
+    case $::operatingsystem {
+        Ubuntu: {
+            package {
+                # Built from https://github.com/rail/android-sdk
+                'android-sdk18':
+                    ensure => '0.r18-0moz1';
+            }
+        }
+
+        default: {
+            fail("cannot install on $::operatingsystem")
+        }
+    }
+}
