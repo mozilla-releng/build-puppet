3b4c17fb110fb9decd5c59fc67317c4687a08b83
John Hopkins <jhopkins@mozilla.com>
2013-09-12 16:01:16 -0400

    Bump repoflag to clear the yum cache for bug 914476.

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -58,7 +58,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 9
+            $repoflag = 10
             file {
                 "/etc/.repo-flag":
                     content =>
