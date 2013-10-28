71c5d7c4d7ebea32cebbfefda53240ef90fc0f0c
Armen Zambrano Gasparnian <armenzg@mozilla.com>
2013-10-28 13:51:59 -0400

    Fix typo. r=bustage

--- modules/talos/manifests/init.pp
+++ modules/talos/manifests/init.pp
@@ -38,6 +38,7 @@ class talos {
                         "libvirt-bin":
                             ensure => "absent"
                     }
+                }
             }
         }
         Darwin: {
