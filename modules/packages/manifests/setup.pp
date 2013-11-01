1dd8901a77c1adcefa8227a7981dac91e2749a84
Dustin J. Mitchell <dustin@mozilla.com>
2013-11-01 13:49:05 -0400

    Bug 931875: bump repoflag; a=bustage

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -93,7 +93,7 @@ class packages::setup {
             }
             # to flush the package index, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 5
+            $repoflag = 6
             file {
                 "/etc/.repo-flag":
                     content =>
