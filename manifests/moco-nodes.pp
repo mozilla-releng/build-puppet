2f5e57562268c8f5b9ab85c51f1c783f669e889e
Rail Aliiev <rail@mozilla.com>
2013-11-27 10:05:51 -0500

    Bug 936460 - Run selfserve-agent on multiple masters. r=Callek

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -273,6 +273,7 @@ node "buildbot-master62.srv.releng.use1.mozilla.com" {
             basedir => "build1";
     }
     include toplevel::server::buildmaster
+    include selfserve_agent
 }
 
 node "buildbot-master63.srv.releng.use1.mozilla.com" {
@@ -303,6 +304,7 @@ node "buildbot-master65.srv.releng.usw2.mozilla.com" {
             basedir => "build1";
     }
     include toplevel::server::buildmaster
+    include selfserve_agent
 }
 
 node "buildbot-master66.srv.releng.usw2.mozilla.com" {
