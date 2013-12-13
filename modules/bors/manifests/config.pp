54a1e9cc76ed4613fd0cd1ea52e532e849b8c6cf
Dustin J. Mitchell <dustin@mozilla.com>
2013-12-13 16:55:44 -0500

    Bug 950215: add copyright headers; r=dividehex

--- modules/bors/manifests/config.pp
+++ modules/bors/manifests/config.pp
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 define bors::config($basedir, $owner, $group, $repo_owner, $repo, $reviewers,
                     $builders, $buildbot_url, $bors_user, $bors_pass,
                     $test_ref, $master_ref, $n_builds = "5") {
