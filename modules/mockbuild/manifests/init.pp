28496929a9f3cad60e0a301a37c309cccbe316e8
Rail Aliiev <rail@mozilla.com>
2014-01-22 09:14:13 -0500

    Bug 962383 - Put /builds/mock_mozilla under puppet's control. r=dustin

--- modules/mockbuild/manifests/init.pp
+++ modules/mockbuild/manifests/init.pp
@@ -20,6 +20,15 @@ class mockbuild {
             require => Class["packages::mozilla::mock_mozilla"],
             source => "puppet:///modules/mockbuild/mozilla-centos6-x86_64.cfg",
             notify => Exec['mock_clean_mozilla-centos6-x86_64'];
+        # make sure that the directory exists for cases when
+        # /builds/mock_mozilla is mounted on a non persistent volumes (like EC2
+        # instance storage)
+        "/builds/mock_mozilla":
+            require => Class["packages::mozilla::mock_mozilla"],
+            ensure  => "directory",
+            owner   => "root",
+            group   => "mock_mozilla",
+            mode    => 2775;
     }
     exec {
         'mock_clean_mozilla-centos6-i386':
