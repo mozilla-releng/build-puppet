e480fa78c9a8808f189c28ff88dca607587abfda
Dustin J. Mitchell <dustin@mozilla.com>
2014-03-05 09:10:44 -0800

    Bug 946872: Upgrade facter and puppet on CentOS; r=Callek

--- modules/packages/manifests/puppet.pp
+++ modules/packages/manifests/puppet.pp
@@ -7,24 +7,32 @@ class packages::puppet {
         'packages::puppet::end': ;
     }
 
-    $puppet_version = "3.2.2"
-    $puppet_dmg_version = "${puppet_version}"
-    $puppet_rpm_version = "${puppet_version}-1.el6"
-    $puppet_deb_version = "${puppet_version}-1puppetlabs1"
-    $facter_version = "1.7.1"
+    $new_puppet_version = "3.4.2"
+    $old_puppet_version = "3.2.2"
+    $puppet_dmg_version = "${old_puppet_version}"
+    $puppet_rpm_version = "${new_puppet_version}-1.el6"
+    $puppet_deb_version = "${old_puppet_version}-1puppetlabs1"
+    $old_facter_version = "1.7.1"
+    $new_facter_version = "1.7.5"
+    $facter_dmg_version = "${old_facter_version}"
+    $facter_rpm_version = "${new_facter_version}-1.el6"
+    $facter_deb_version = "${old_facter_version}-1puppetlabs1"
 
     case $::operatingsystem {
         CentOS: {
             package {
                 "puppet":
                     ensure => "$puppet_rpm_version";
-                # puppet this pulls the required version of facter
+                "facter":
+                    ensure => "$facter_rpm_version";
             }
         }
         Ubuntu: {
             package {
                 ["puppet", "puppet-common"]:
                     ensure => "$puppet_deb_version";
+                ["facter"]:
+                    ensure => "$facter_deb_version";
             }
         }
         Darwin: {
@@ -36,7 +44,7 @@ class packages::puppet {
                     version => $puppet_dmg_version;
                 facter:
                     os_version_specific => false,
-                    version => $facter_version;
+                    version => $facter_dmg_version;
             } -> Anchor['packages::puppet::end']
         }
         Windows: {
