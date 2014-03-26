681fb62a1c5aa8a88573371d363f678c33e0212a
Justin Wood <Callek@gmail.com>
2014-03-25 22:50:38 -0400

    Bug 921067 - Bump slaveapi ver for panda support. r=nthomas
    --HG--
    extra : rebase_source : 9f32b42ca91b66412278bee52da9cf8c6b924230

--- modules/slaveapi/manifests/instance.pp
+++ modules/slaveapi/manifests/instance.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define slaveapi::instance($listenaddr, $port, $version="1.0.16") {
+define slaveapi::instance($listenaddr, $port, $version="1.0.17") {
     include config
     include slaveapi::base
     include users::builder
@@ -59,6 +59,7 @@ define slaveapi::instance($listenaddr, $port, $version="1.0.16") {
                 "orderedmultidict==0.7.1",
                 "pytz==2013.7",
                 "slaveapi==${version}",
+                "mozpoolclient==0.1.5",
             ];
     }
 
