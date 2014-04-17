33ba224097fdbb6611e47e9bba39fec2336c8574
Henrik Skupin <hskupin@mozilla.com>
2014-04-17 11:28:15 -0400

    Bug 997748 - Adding mozmill-ci staging nodes a=orgonly

--- manifests/qa-nodes.pp
+++ manifests/qa-nodes.pp
@@ -9,3 +9,55 @@ node "puppetmaster1.qa.scl3.mozilla.com" {
 node "mm-ub-1204-32-temp.qa.scl3.mozilla.com" {
     include toplevel::base
 }
+
+node "mm-osx-106.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-osx-107.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-osx-108.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-osx-109.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-ub-1204-32.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-ub-1204-64.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-ub-1310-32.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-ub-1310-64.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-win-xp-32.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-win-7-32.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-win-7-64.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-win-81-32.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mm-win-81-64.qa.scl3.mozilla.com" {
+    include toplevel::base
+}
