fbd9c52e545d91786b048516c07677814d27ece6
Rail Aliiev <rail@mozilla.com>
2014-05-09 13:51:36 -0400

    Bug 1007967 - Unable to puppetize a fresh tst-linux64-ec2 slave for loan. r=Callek

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -95,7 +95,7 @@ class packages::setup {
             }
             # to flush the package index, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 12
+            $repoflag = 13
             file {
                 "/etc/.repo-flag":
                     content =>
