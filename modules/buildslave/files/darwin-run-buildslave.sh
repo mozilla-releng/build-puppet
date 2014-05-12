f653742c187f086a247b613828993effab6007bc
Chris Cooper <ccooper@deadsquid.com>
2014-05-12 09:20:11 -0400

    Bug 880003 - Cleanup temp files on Darwin before starting buildbot - r=Callek

--- modules/buildslave/files/darwin-run-buildslave.sh
+++ modules/buildslave/files/darwin-run-buildslave.sh
@@ -6,5 +6,13 @@
 # remove the semaphore file that was causing this process to run
 rm -rf /var/tmp/semaphore/run-buildbot
 
+# Pre-flight cleanup
+if [ "${TMPDIR}" != "" ]; then
+  rm -rf ${TMPDIR}/*
+fi
+rm -rf /Users/cltbld/Library/Caches/TemporaryItems/*
+rm -rf /Users/cltbld/.Trash/*
+rm -rf /Users/cltbld/Downloads/*
+
 logger -ist run-buildslave "starting runslave.py"
 exec /usr/bin/python /usr/local/bin/runslave.py
