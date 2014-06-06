c1e6dd52c3415275b6a51c4bc9eb3491482320e9
Henrik Skupin <mail@hskupin.info>
2014-06-06 17:37:43 +0200

    Remove accidentally added testing boxes. a=org-only

--- manifests/qa-nodes.pp
+++ manifests/qa-nodes.pp
@@ -2,15 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-node "localhost.fritz.box" {
-    include toplevel::server::puppetmaster
-}
-
-
-node "vagrant-ubuntu-trusty-64.fritz.box" {
-    include toplevel::slave::qa::mozmill_ci
-}
-
 # Temporary nodes for testing puppetagain on QA infrastructure
 
 node "mm-ub-1204-32-temp.qa.scl3.mozilla.com" {
