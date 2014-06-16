3239c16a3b1572602290b547321bff1bc4f2381d
Rail Aliiev <rail@mozilla.com>
2014-06-16 10:05:59 -0400

    Backout Bug 1023477 - reduce EBS writes by removing journal, tweaking writeout. r=me

--- modules/toplevel/manifests/slave/build.pp
+++ modules/toplevel/manifests/slave/build.pp
@@ -13,7 +13,6 @@ class toplevel::slave::build inherits toplevel::slave {
 
     include ntp::daemon
     include tweaks::nofile
-    include tweaks::filesystem
 
     include nrpe
     include nrpe::check::buildbot
