9cc0d56fa83a5492a4d6e1bf1390c0e22e53284e
Dustin J. Mitchell <dustin@mozilla.com>
2014-06-20 09:56:09 -0400

    No bug: fix servo toplevel classes; a=bustage

--- manifests/servo-nodes.pp
+++ manifests/servo-nodes.pp
@@ -7,18 +7,18 @@
 node /servo-.*\.build\.servo\.aws-.*\.mozilla\.com/ {
     # Make sure we get our /etc/hosts set up
     $slave_trustlevel = "prod"
-    include toplevel::slave::build::mock
+    include toplevel::slave::releng::build::mock
 }
 
 node /servo-.*\.build\.servo\.releng\.(use1|usw2)\.mozilla.com/ {
     # Make sure we get our /etc/hosts set up
     $slave_trustlevel = "prod"
-    include toplevel::slave::build::mock
+    include toplevel::slave::releng::build::mock
 }
 
 node /servo-lion-r5-\d+\.build\.servo\.releng\.scl3\.mozilla\.com/ {
     $slave_trustlevel = "prod"
-    include toplevel::slave::build::standard
+    include toplevel::slave::releng::build::standard
 }
 
 
