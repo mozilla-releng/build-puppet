04c02d98af3984d11d64f60b61d7eea1dcf35fee
Henrik Skupin <mail@hskupin.info>
2014-06-30 17:00:44 +0200

    Bug 997721 - Add proxy support for gui applications on OS X. r=dustin

--- modules/web_proxy/manifests/environment.pp
+++ modules/web_proxy/manifests/environment.pp
@@ -3,7 +3,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class web_proxy::environment {
-    if ($web_proxy::url != '') {
+    if $web_proxy::host != "" {
         case $operatingsystem {
             Darwin,
             CentOS,
