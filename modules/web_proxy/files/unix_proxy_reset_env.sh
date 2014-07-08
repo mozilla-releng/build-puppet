4ade3eccfefbeab629198a9b8e67c31175db319a
Henrik Skupin <mail@hskupin.info>
2014-07-08 17:52:17 +0200

    Bug 1032133 - Don't inherit environment variables for puppet agent runs. r=dustin

---
+++ modules/web_proxy/files/unix_proxy_reset_env.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+unset http_proxy
+unset https_proxy
+unset ftp_proxy
+unset no_proxy
+
+unset HTTP_PROXY
+unset HTTPS_PROXY
+unset FTP_PROXY
+unset NO_PROXY
