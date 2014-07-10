4ecbf6ca958c703f362235560bf81c113a4726f4
Mark Cornmesser <mcornmesser@mozilla.com>
2014-07-10 10:32:02 -0700

    BUG 1037040 Python package manifests Windows support

--- modules/packages/manifests/mozilla/tooltool.pp
+++ modules/packages/manifests/mozilla/tooltool.pp
@@ -2,21 +2,33 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class packages::mozilla::tooltool {
-    include packages::mozilla::python27
-    include users::root
     # this package is simple enough that its source code is embedded in the
     # puppet repo.  It gets the Python intepreter added to its shebang line
     $python = $::packages::mozilla::python27::python
-    file {
-        "/tools/tooltool.py":
-            owner => $users::root::username,
-            group => $users::root::group,
-            mode => 0755,
-            content => template("packages/tooltool.py");
-    }
-    file {
-        "/builds/tooltool.py":
-            ensure => "link",
-            target => "/tools/tooltool.py";
+    case $::operatingsystem {
+        Windows: {
+        include packages::mozilla::mozilla_build
+            file{
+                "C:/mozilla-build/tooltool.py":
+                    require => Class["packages::mozilla::mozilla_build"],
+                    content => template("packages/tooltool.py");
+            }
+        }
+        default: {
+        include packages::mozilla::python27
+        include users::root
+            file {
+                "/tools/tooltool.py":
+                    owner => $users::root::username,
+                    group => $users::root::group,
+                    mode => 0755,
+                    content => template("packages/tooltool.py");
+            }
+            file {
+                "/builds/tooltool.py":
+                    ensure => "link",
+                    target => "/tools/tooltool.py";
+            }
+        }
     }
 }
---
+++ modules/packages/manifests/psutil.pp
@@ -0,0 +1,16 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# All buildbot slaves (both build and test) are subclasses of this class.
+
+class packages::psutil {
+    #Python Psutil original package available at https://pypi.python.org/packages/2.7/p/psutil/psutil-2.1.1.win32-py2.7.exe#md5=5d51ac4843a4cc8407d0be0e2d3d0eac
+    #This package has been manual repackage using 7zip. 
+    #The reason for the repackaging is some directory structure has been reorganized to prevent other processes from breaking.
+    #Ref. https://bugzilla.mozilla.org/show_bug.cgi?id=918992
+    exec { "psutil":
+        creates => 'C:\mozilla-build\sitepackages\_psutil_mswindows.pyd',
+        require => Class["packages::7z920"],
+        command => '"C:\Program Files (x86)\7-Zip\7z.exe" x C:\InstallerSource\puppetagain.pub.build.mozilla.org\ZIPs\psutil.zip -oC:\mozilla-build\sitepackages -y'
+    }
+}  
