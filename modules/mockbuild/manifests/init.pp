6e6a989a93c87d61b1739fd218b4335501d505e8
Dustin J. Mitchell <dustin@mozilla.com>
2014-07-10 14:13:45 -0400

    Bug 1022763: configure mockbuild via runner; r=simone
    This configures mockbuild based on the location of the node, but in such
    a way that puppet is not required.  Instead the configuration is based
    on the placement/availability-zone instance metadata, and if that is
    empty (e.g., in-house), then uses the public vhost,
    http://mockbuild-repos.pub.build.mozilla.org.

--- modules/mockbuild/manifests/init.pp
+++ modules/mockbuild/manifests/init.pp
@@ -6,24 +6,6 @@ class mockbuild {
     include mockbuild::services
 
     file {
-        "/etc/mock_mozilla/mozilla-f16-x86_64.cfg":
-            require => Class["packages::mozilla::mock_mozilla"],
-            source => "puppet:///modules/mockbuild/mozilla-f16-x86_64.cfg";
-        "/etc/mock_mozilla/mozilla-f16-i386.cfg":
-            require => Class["packages::mozilla::mock_mozilla"],
-            source => "puppet:///modules/mockbuild/mozilla-f16-i386.cfg";
-        "/etc/mock_mozilla/mozilla-centos6-i386.cfg":
-            require => Class["packages::mozilla::mock_mozilla"],
-            source => "puppet:///modules/mockbuild/mozilla-centos6-i386.cfg",
-            notify => Exec['mock_clean_mozilla-centos6-i386'];
-        "/etc/mock_mozilla/mozilla-centos6-x86_64.cfg":
-            require => Class["packages::mozilla::mock_mozilla"],
-            source => "puppet:///modules/mockbuild/mozilla-centos6-x86_64.cfg",
-            notify => Exec['mock_clean_mozilla-centos6-x86_64'];
-        "/etc/mock_mozilla/mozilla-centos6-x86_64-android.cfg":
-            require => Class["packages::mozilla::mock_mozilla"],
-            source => "puppet:///modules/mockbuild/mozilla-centos6-x86_64-android.cfg",
-            notify => Exec['mock_clean_mozilla-centos6-x86_64'];
         # make sure that the directory exists for cases when
         # /builds/mock_mozilla is mounted on a non persistent volumes (like EC2
         # instance storage)
@@ -33,22 +15,5 @@ class mockbuild {
             owner   => "root",
             group   => "mock_mozilla",
             mode    => 2775;
-    }
-    exec {
-        'mock_clean_mozilla-centos6-i386':
-            # this will expire all mock caches
-            command => "/usr/bin/sudo -u $::config::builder_username /usr/bin/mock_mozilla -v -r mozilla-centos6-i386 --scrub=all",
-            onlyif => "/usr/bin/test -e /builds/mock_mozilla/mozilla-centos6-i386/result/state.log",
-            refreshonly => true;
-        'mock_clean_mozilla-centos6-x86_64':
-            # this will expire all mock caches
-            command => "/usr/bin/sudo -u $::config::builder_username /usr/bin/mock_mozilla -v -r mozilla-centos6-x86_64 --scrub=all",
-            onlyif => "/usr/bin/test -e /builds/mock_mozilla/mozilla-centos6-x86_64/result/state.log",
-            refreshonly => true;
-        'mock_clean_mozilla-centos6-x86_64-android':
-            # this will expire all mock caches
-            command => "/usr/bin/sudo -u $::config::builder_username /usr/bin/mock_mozilla -v -r mozilla-centos6-x86_64-android --scrub=all",
-            onlyif => "/usr/bin/test -e /builds/mock_mozilla/mozilla-centos6-x86_64-android/result/state.log",
-            refreshonly => true;
     }
 }
