23aa7ab10f14f81c09c8caf67536fa65329b393b
Rail Aliiev <rail@mozilla.com>
2014-07-10 12:42:58 -0400

    Bug 1035834 - aws_stop_idle.py and aws_watch_pending.py should be restarted if they hung. r=dustin

--- modules/aws_manager/manifests/crontask.pp
+++ modules/aws_manager/manifests/crontask.pp
@@ -3,13 +3,16 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 define aws_manager::crontask($ensure, $virtualenv_dir, $user, $cwd, $params='',
                              $hour='*', $minute='*', $month='*', $monthday='*',
-                             $weekday='*', $script=$title, $subject=$title) {
+                             $weekday='*', $script=$title, $subject=$title,
+                             $process_timeout=0) {
     include cron
     include ::config
 
     $cronfile = "/etc/cron.d/aws_manager-$title.cron"
     $cronscript = "${virtualenv_dir}/bin/aws_manager-$title.sh"
     $lockdir = "${virtualenv_dir}"
+    $monitoring_cronfile = "/etc/cron.d/aws_manager-$title-monitor.cron"
+    $monitoring_cronscript = "${virtualenv_dir}/bin/aws_manager-$title-monitor.sh"
 
     case $ensure {
         present: {
@@ -20,10 +23,19 @@ define aws_manager::crontask($ensure, $virtualenv_dir, $user, $cwd, $params='',
                     content => template("${module_name}/crontask.erb"),
                     mode => 0755;
             }
+            if ($process_timeout > 0){
+                file {
+                    $monitoring_cronfile:
+                        content => "$minute $hour $monthday $month $weekday $user $monitoring_cronscript 2>&1 | mail -E -s '[aws cron] $subject monitor' ${::config::aws_manager_mail_to}\n";
+                    $monitoring_cronscript:
+                        content => template("${module_name}/kill_long_running.sh.erb"),
+                        mode => 0755;
+                }
+            }
         }
         absent: {
             file {
-                [$cronfile, $cronscript]:
+                [$cronfile, $cronscript, $monitoring_cronfile, $monitoring_cronscript]:
                     ensure => absent;
             }
         }
