d5f49ece9ec5a87d932c1477973fb9920f1c70fb
Mark Cornmesser <mcornmesser@mozilla.com>
2014-07-18 08:53:57 -0700

    BUG 1040822 Hardware module Windows support; r=dustin

---
+++ modules/hardware/manifests/highperformance.pp
@@ -0,0 +1,17 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# All buildbot slaves (both build and test) are subclasses of this class.
+
+#Set Power option to High Performance. Refer to http://technet.microsoft.com/en-us/library/cc748940%28v=ws.10%29.aspx
+class hardware::highperformance {
+    exec { "highperformance":
+        creates => 'C:\programdata\PuppetLabs\puppet\var\lib\highperformance.txt',
+        command => '"C:\Windows\System32\powercfg.exe" -setactive 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c',
+    }
+    file {
+            'C:\programdata/PuppetLabs/puppet/var/lib/highperformance.txt':
+                require => Exec["highperformance"],
+                content => "Semaphore",        
+    }
+}  
