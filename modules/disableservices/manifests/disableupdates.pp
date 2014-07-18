5cad4dc4d088edb93308f152c880c4349bbc10a9
Mark Cornmesser <mcornmesser@mozilla.com>
2014-07-18 08:43:40 -0700

    BUG 1040819 Disable Services module Windows support; r=dustin

---
+++ modules/disableservices/manifests/disableupdates.pp
@@ -0,0 +1,35 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# All buildbot slaves (both build and test) are subclasses of this class.
+
+#Disables Automatic Windows Updates
+#Approved Updates are downloaded and applied during the MDT installation
+
+class disableservices::disableupdates {
+    registry::value { 'NoActiveDesktop':
+        key    => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer',
+        type   => dword,
+        data   => '00000001',
+    }
+    registry::value { 'NoActiveDesktopChanges':
+        key    => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer',
+        type   => dword,
+        data   => '00000001',
+    }    
+    registry::value { 'ForceActiveDesktopOn':
+        key    => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer',
+        type   => dword,
+        data   => '00000000',
+    }
+    registry::value { 'ShowSuperHidden':
+        key    => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer',
+        type   => dword,
+        data   => '00000001',
+    }
+    registry::value { 'NoWindowsUpdate':
+        key    => 'HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer',
+        type   => dword,
+        data   => '00000001',
+    }
+}
