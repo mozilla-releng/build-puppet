2923bfbb7467c7973aca3dc38049da4dd2dd1663
Dustin J. Mitchell <dustin@mozilla.com>
2014-07-21 08:24:52 -0400

    Bug 1040819, 1040822: remove copy/pasted comment; a=NPOTB

--- modules/hardware/manifests/highperformance.pp
+++ modules/hardware/manifests/highperformance.pp
@@ -1,7 +1,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# All buildbot slaves (both build and test) are subclasses of this class.
 
 #Set Power option to High Performance. Refer to http://technet.microsoft.com/en-us/library/cc748940%28v=ws.10%29.aspx
 class hardware::highperformance {
