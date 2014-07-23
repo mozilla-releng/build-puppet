c442b466ea14c66a4122bdf26c5adeec87069871
Mark Cornmesser <mcornmesser@mozilla.com>
2014-07-23 12:02:16 -0700

    Bug 1040584: Custom Windows MSI Provider; r=dustin

---
+++ modules/packages/manifests/pkgmsi.pp
@@ -0,0 +1,25 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+define packages::pkgmsi($package=$title, $msi, $private=false, $install_options=[]) {
+    include dirs::installersource::puppetagain_pub_build_mozilla_org::msis
+    $installersource_posix = 'c:/InstallerSource/puppetagain.pub.build.mozilla.org'
+    $installersource_win = 'c:\InstallerSource\puppetagain.pub.build.mozilla.org'
+    $p = $private ? {
+        true => "/private",
+        false => ""
+    }
+    file {
+            "$installersource_posix/MSIs/$msi":
+                source   => "puppet:///repos$p/MSIs/$msi",
+                require  => Class["dirs::installersource::puppetagain_pub_build_mozilla_org::msis"],
+    }
+    package {
+            $package:
+                ensure => installed,
+                provider => windows,
+                source => "$installersource_win\\MSIs\\$msi",
+                require => File["$installersource_posix/MSIs/$msi"];
+    }
+}
