fc062411f6d117eaa67821675a24f0beaba5d731
Dustin J. Mitchell <dustin@mozilla.com>
2014-07-29 13:08:15 -0400

    Bug 997729: check puppetmaster certificates via nagios; r=dividehex

---
+++ modules/nrpe/manifests/check/puppetmaster_certs.pp
@@ -0,0 +1,17 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+class nrpe::check::puppetmaster_certs {
+    include nrpe::settings
+    $plugins_dir = $nrpe::settings::plugins_dir
+
+    nrpe::check {
+        'check_puppetmaster_certs':
+            cfg => "$plugins_dir/check_puppetmaster_certs";
+    }
+
+    nrpe::plugin {
+        'check_puppetmaster_certs': ;
+    }
+}
+
