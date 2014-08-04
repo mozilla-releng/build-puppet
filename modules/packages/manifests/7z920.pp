31389e656e82cf53865789a8c966945ed3db8f6a
Mark Cornmesser <mcornmesser@mozilla.com>
2014-08-04 08:56:39 -0700

    BUG 1045668 Windows MSI packages support; r=dustin
    +whitespace fixup (dustin)

--- modules/packages/manifests/7z920.pp
+++ modules/packages/manifests/7z920.pp
@@ -1,13 +1,11 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class packages::7z920 {
     # Source comes from http://www.7-zip.org/
-    #This is required to install Mozilla build package
-    package { "7z920.msi":
-        ensure          => installed,
-        source          => 'C:\InstallerSource\puppetagain.pub.build.mozilla.org\msis\7z920.msi',
-        install_options => ['/quiet'],
+    # This is required to install Mozilla build package
+    packages::pkgmsi {"7z920.msi":
+        msi             => "7z920.msi",
+        install_options => ['/quiet'];
     }
  }
-
---
+++ modules/packages/manifests/nsclient_plus_plus.pp
@@ -0,0 +1,10 @@
+#This Source Code Form is subject to the terms of the Mozilla Public
+#License, v. 2.0. If a copy of the MPL was not distributed with this
+#file, You can obtain one at http://mozilla.org/MPL/2.0/.
+class packages::nsclient_plus_plus{
+        # source http://nsclient.org/nscp/downloads
+        packages::pkgmsi { "NSClient++-0.3.9-x64.msi":
+            msi             => "NSClient++-0.3.9-x64.msi",
+            install_options => ['/QUIET'];
+        }
+}   
