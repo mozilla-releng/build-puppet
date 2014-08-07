5efce3c7f4d18119649848f34609114cf063063c
Mark Cornmesser <mcornmesser@mozilla.com>
2014-08-07 09:21:57 -0700

    BUG 1048962 Custom resource packages::pkgzip; r=dustin

---
+++ modules/packages/manifests/pkgzip.pp
@@ -0,0 +1,27 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+define packages::pkgzip($package=$title, $zip, $private=false, $target_dir) {
+    include dirs::installersource::puppetagain_pub_build_mozilla_org::zips
+    include packages::7z920
+    $installersource_posix = 'c:/InstallerSource/puppetagain.pub.build.mozilla.org'
+    $installersource_win = 'c:\InstallerSource\puppetagain.pub.build.mozilla.org'
+    $quoted_7zip = '"C:\Program Files (x86)\7-Zip\7z.exe"'
+    $p = $private ? {
+        true => "/private",
+        false => ""
+    }
+    file {
+            "$installersource_posix/ZIPs/$zip":
+                source  => "puppet:///repos$p/ZIPs/$zip",
+                require => Class["dirs::installersource::puppetagain_pub_build_mozilla_org::zips"];
+    }
+    shared::execonce {
+            $zip :
+                command    => "$quoted_7zip x $installersource_win\\ZIPs\\$zip -o$target_dir -y",
+                require    => [File["$installersource_posix/ZIPs/$zip"],
+                                Class["packages::7z920"]
+                                        ];
+    }
+}
