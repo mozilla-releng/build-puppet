472ca960a0e15e07fc898eef576bf64fbdde075b
Mark Cornmesser <mcornmesser@mozilla.com>
2014-08-11 08:42:41 -0700

    BUG 1048766 Disabel Server initialization logon Window; r=dustin

---
+++ modules/tweaks/manifests/server_initialize.pp
@@ -0,0 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Disable Server Initialization logon window
+class tweaks::server_initialize {
+    # data 1 = enable  data 0 = disabled
+    registry::value { 'DoNotOpenInitialConfigurationTasksAtLogon':
+        key    => 'HKLM\SOFTWARE\Microsoft\ServerManager\Oobe',
+        type   => dword,
+        data   => '1',
+    }
+}
