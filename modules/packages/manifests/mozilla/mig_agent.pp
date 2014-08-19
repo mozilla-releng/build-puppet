44bf8cf5e5958cc1eb9d6550a40ac0e78c16a02a
Julien Vehent <jvehent@mozilla.com>
2014-08-19 11:54:27 -0400

    Bug 1041577: toplevel::server conditional for mig::agent inclusion; r=dustin

--- modules/packages/manifests/mozilla/mig_agent.pp
+++ modules/packages/manifests/mozilla/mig_agent.pp
@@ -2,26 +2,23 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class packages::mozilla::mig_agent {
-    include ::config
-    if ($::config::enable_mig_agent) {
-        case $::operatingsystem {
-            'CentOS', 'RedHat': {
-                realize(Packages::Yumrepo['mig-agent'])
-                package {
-                    # The package starts the mig-agent service on install
-                    # the agent created the necessary service files for (upstart|systemd|sysvinit)
-                    # where needed. Agent init process is described at
-                    # https://github.com/mozilla/mig/blob/master/doc/concepts.rst
-                    'mig-agent':
-                        ensure => '201408181131+68245f0.prod-1'
-                }
+    case $::operatingsystem {
+        'CentOS', 'RedHat': {
+            realize(Packages::Yumrepo['mig-agent'])
+            package {
+                # The package starts the mig-agent service on install
+                # the agent created the necessary service files for (upstart|systemd|sysvinit)
+                # where needed. Agent init process is described at
+                # https://github.com/mozilla/mig/blob/master/doc/concepts.rst
+                'mig-agent':
+                    ensure => '201408181131+68245f0.prod-1'
             }
-            'Ubuntu': {
-                realize(Packages::Aptrepo['mig-agent'])
-                package {
-                    'mig-agent':
-                        ensure => '201408181131+68245f0.prod'
-                }
+        }
+        'Ubuntu': {
+            realize(Packages::Aptrepo['mig-agent'])
+            package {
+                'mig-agent':
+                    ensure => '201408181131+68245f0.prod'
             }
         }
     }
