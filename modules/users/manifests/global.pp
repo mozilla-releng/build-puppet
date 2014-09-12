d0a5a3d7ad028dd0de9250b76010af89b3423fc1
Jake Watkins <jwatkins@mozilla.com>
2014-09-12 11:32:37 -0700

    Bug 1046877: backing out bustage; a=bustage

--- modules/users/manifests/global.pp
+++ modules/users/manifests/global.pp
@@ -10,16 +10,27 @@ class users::global {
 	    content => "export TMOUT=86400";  # Shells timeout after 1 day
     }
 
-    # On OS X, the Administrator user is created at system install time.
-    # We want to keep this around as it is critical to some operations on newer
-    # OSX versions
+    # On OS X, the Administrator user is created at system install time.  We
+    # don't want to keep it around, except on Mavericks (10.9, and by the look
+    # of the error messages future versions too), where it seems critical to
+    # proper system operation.
     if ($::operatingsystem == "Darwin") {
-        user {
-            "administrator":
-                ensure => present,
-                password => secret("root_pw_pbkdf2"),
-                salt => secret("root_pw_pbkdf2_salt"),
-                iterations => secret("root_pw_pbkdf2_iterations");
+        case $macosx_productversion_major {
+            10.6,10.7,10.8: {
+                darwinuser {
+                    "administrator":
+                        ensure => absent;
+                }
+            }
+            default: {
+                darwinuser {
+                    "administrator":
+                        ensure => present,
+                        password => secret("root_pw_pbkdf2"),
+                        salt => secret("root_pw_pbkdf2_salt"),
+                        iterations => secret("root_pw_pbkdf2_iterations");
+                }
+            }
         }
     }
 
