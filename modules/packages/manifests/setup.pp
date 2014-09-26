ab181e77f3dd354d8a19628eabce6770c00651d8
Rail Aliiev <rail@mozilla.com>
2014-09-26 14:09:58 -0400

    Bug 1072465 - Upgrade bash on all Centos and Ubuntu machines. r=jwatkins

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -107,7 +107,7 @@ class packages::setup {
             }
             # to flush the package index, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 16
+            $repoflag = 17
             file {
                 "/etc/.repo-flag":
                     content =>
@@ -166,6 +166,10 @@ class packages::setup {
                     components   => ["all"];
                 "openssl":
                     url_path     => "repos/apt/custom/openssl",
+                    distribution => "${lsbdistcodename}",
+                    components   => ["all"];
+                "bash":
+                    url_path     => "repos/apt/custom/bash",
                     distribution => "${lsbdistcodename}",
                     components   => ["all"];
             }
