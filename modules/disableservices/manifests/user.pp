6e8d6456e313a30f2135964749dda4eac6db9eb6
Dustin J. Mitchell <dustin@mozilla.com>
2014-10-20 14:49:13 -0700

    Bug 1078882: disable the iCloud welcome screen; r=whimboo

--- modules/disableservices/manifests/user.pp
+++ modules/disableservices/manifests/user.pp
@@ -7,7 +7,7 @@ class disableservices::user($username, $group, $home) {
     case $::operatingsystem {
         Darwin : {
             osxutils::defaults {
-                'builder-disablescreensaver':
+                '${username}-disablescreensaver':
                     domain => "${home}/Library/Preferences/ByHost/com.apple.screensaver.$sp_platform_uuid",
                     key => "idleTime",
                     value => "0";
@@ -38,10 +38,29 @@ class disableservices::user($username, $group, $home) {
             # and to make double-sure, turn off quarantining:
             # http://superuser.com/questions/38658/how-to-suppress-repetition-of-warnings-that-an-application-was-downloaded-from-t
             osxutils::defaults {
-                "builder-disable-quarantine":
+                "${username}-disable-quarantine":
                     domain => "${home}/Library/Preferences/com.apple.LaunchServices.plist",
                     key => "LSQuarantine",
                     value => "0";
+            }
+
+            # disable the iCloud setup, and a few others, per
+            # http://derflounder.wordpress.com/2013/10/27/disabling-the-icloud-sign-in-pop-up-message-on-lion-and-later/
+            if ($::macosx_productversion_major == "10.10") {
+                osxutils::defaults {
+                    "${username}-disable-icloud":
+                        domain => "${home}/Library/Preferences/com.apple.SetupAssistant.plist",
+                        key => "DidSeeCloudSetup",
+                        value => "1";
+                    "${username}-disable-icloud-version":
+                        domain => "${home}/Library/Preferences/com.apple.SetupAssistant.plist",
+                        key => "LastSeenCloudProductVersion",
+                        value => "${::macosx_productversion_major}";
+                    "${username}-disable-gesture-movie":
+                        domain => "${home}/Library/Preferences/com.apple.SetupAssistant.plist",
+                        key => "GestureMovieSeen",
+                        value => "none";
+                }
             }
         }
     }
