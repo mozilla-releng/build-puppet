515a41ade5e65e286369edde45fa95adc4bf190b
Mark Cornmesser <mcornmesser@mozilla.com>
2014-10-30 08:54:13 -0700

    Bug 1048962: Windows working directory for pkgzip packages; r=dustin

---
+++ modules/dirs/manifests/installersource/puppetagain_pub_build_mozilla_org/zips.pp
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+class dirs::installersource::puppetagain_pub_build_mozilla_org::zips {
+    include dirs::installersource::puppetagain_pub_build_mozilla_org
+    file {
+        "$env_systemdrive/installersource/puppetagain.pub.build.mozilla.org/ZIPs":
+            ensure => directory,
+    }
+}
