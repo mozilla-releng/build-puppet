0c67c88fd88bddd4203d31e38c185477241fc323
Mark Cornmesser <mcornmesser@mozilla.com>
2014-10-31 08:55:42 -0700

    Bug 1086998: Tweaks Module Windows Support; r=dustin

---
+++ modules/tweaks/manifests/pwrshell_options.pp
@@ -0,0 +1,10 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class tweaks::pwrshell_options {
+    #Set powershell option to allow all poershell scripts to be ran. Refer to http://technet.microsoft.com/en-us/library/ee176961.aspx.
+    shared::execonce { "pwrshellopt":
+        command => '"C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -noprofile Set-ExecutionPolicy Unrestricted',
+    }
+}
