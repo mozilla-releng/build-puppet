9f7299bf526d75fd8edbf3d0a7f5f70b62a42d5b
Mark Cornmesser <mcornmesser@mozilla.com>
2014-11-14 09:42:50 -0800

    BUG1096613-corrections in Windows firewall manifest replace firewall with fw r=dustin

--- modules/fw/manifests/windows_settings.pp
+++ modules/fw/manifests/windows_settings.pp
@@ -2,17 +2,17 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-#Ref. http://technet.microsoft.com/en-us/library/dd734783%28v=ws.10%29.aspx regarding netsh command.
+# Ref. http://technet.microsoft.com/en-us/library/dd734783%28v=ws.10%29.aspx regarding netsh command.
 
-class firewall::windows_settings {
+class fw::windows_settings {
     # Ping allowance is based on icmp settings
-    #Allow ping
+    # Allow ping
     shared::execonce { "AllowPing":
         command =>'"C:\Windows\System32\netsh.exe" firewall set icmpsetting 8'
     }
 
-    #The following allows remote desktop group access through the firewall.
-    #Allow rdp
+    # The following allows remote desktop group access through the firewall.
+    # Allow rdp
     shared::execonce { "AllowRDP":
         command =>'"C:\Windows\System32\netsh.exe" advfirewall firewall set rule group="Remote Desktop" new enable=yes'
     }
