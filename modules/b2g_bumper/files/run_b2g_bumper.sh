70d46255b4778306bddd0838f33c0e6d65796408
Peter Moore <pmoore@mozilla.com>
2014-11-17 18:06:20 +0100

    Bug 1097784 - Caching improvements for b2g_bumper,r=catlee

--- modules/b2g_bumper/files/run_b2g_bumper.sh
+++ modules/b2g_bumper/files/run_b2g_bumper.sh
@@ -4,45 +4,45 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # This file is managed by puppet
-set -e
-cd /builds/b2g_bumper
-exec >> b2g_bumper.log 2>&1
+
+# All references to files/directories used are absolute to simplify
+# troubleshooting, e.g. if someone does a ps to see a process running
+# they can see full paths easily, etc
+
+set -eu
+exec >> /builds/b2g_bumper/b2g_bumper.log 2>&1
 
 function log() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') pid-$$ $*"
 }
 log "Acquiring lock"
-lockfile -s60 -r5 b2g_bumper.lock
-trap "rm -f $PWD/b2g_bumper.lock" EXIT
+lockfile -s60 -r5 /builds/b2g_bumper/b2g_bumper.lock
+trap "rm -f /builds/b2g_bumper/b2g_bumper.lock" EXIT
 
 export PATH=/usr/local/bin:$PATH
+env | grep PATH
 
 # Get mozharness updated / checked out and working
 log "Updating mozharness"
-timeout 300 /usr/local/bin/hgtool.py -b production https://hg.mozilla.org/build/mozharness mozharness
+timeout 300 /usr/local/bin/hgtool.py -b production https://hg.mozilla.org/build/mozharness /builds/b2g_bumper/mozharness
 
-# Start the bumpers in parallel
-pids=""
-for config in master v2.1 v2.0 v1.4 v1.3t; do
-    log "Running b2g bumper ${config}, log in ${PWD}/${config}.log"
-    if [ ! -d $config ]; then
-        mkdir $config
-    fi
-    (cd $config; python ../mozharness/scripts/b2g_bumper.py -c b2g_bumper/${config}.py > ../$config.log 2>&1) &
-    pids="$pids $!"
-done
+# Clear the cache
+rm -f /builds/b2g_bumper/git_ref_cache.json
 
-# Now wait for them to finish
-retval=0
-for pid in $pids; do
-    wait $pid
-    if [ $? != 0 ]; then
-        retval=$?
+# Run the bumpers in sequence
+all_succeeded=true
+find /builds/b2g_bumper/mozharness/configs/b2g_bumper/ -type f -name '*.py' | while read config; do
+    short_name="$(basename "${config}")"
+    log "Running b2g bumper using ${config}, log in /builds/b2g_bumper/${short_name}.log"
+    mkdir -p "/builds/b2g_bumper/${short_name}"
+    if ! python /builds/b2g_bumper/mozharness/scripts/b2g_bumper.py --base-work-dir "/builds/b2g_bumper/${short_name}" -c "${config}.py" --import-git-ref-cache --push-loop --export-git-ref-cache > "/builds/b2g_bumper/${short_name}.log" 2>&1; then
+        all_succeeded=false
     fi
 done
 
-if [ $retval = 0 ]; then
-    # Touch our timestamp file so nagios can check if we're fresh
-    touch b2g_bumper.stamp
-    log "Done"
+if "${all_succeeded}"; then
+    log "All branches succeeded, touching /builds/b2g_bumper/b2g_bumper.stamp so nagios knows we're running ok"
+    touch /builds/b2g_bumper/b2g_bumper.stamp
+else
+    log "Failures! Not updating /builds/b2g_bumper/b2g_bumper.stamp"
 fi
