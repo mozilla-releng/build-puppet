5ce2e267f0c887f3ef99050edc9b180e6f1c6c63
Dustin J. Mitchell <dustin@mozilla.com>
2014-11-20 12:49:53 -0800

    Bug 1096734: add debugging info; r=Callek (irc)

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,9 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# debugging bug 1096734
+warning("fqdn $fqdn operatingsystem $operatingsystem")
+
 import "stages.pp"
 # both of these should be symlnks to the appropriate organization
 import "config.pp"
