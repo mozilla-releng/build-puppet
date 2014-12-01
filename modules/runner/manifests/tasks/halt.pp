194c32a937a6e38780310b2e2a51288bd753eb68
Morgan Phillips <winter2718@gmail.com>
2014-12-01 08:00:35 -0800

    Bug 1103123 - Add a halt task to all runner instances which will reboot machines on failure; r=rail
    Adds the halt class.

---
+++ modules/runner/manifests/tasks/halt.pp
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class runner::tasks::halt() {
+    include runner
+    runner::task {
+        "halt.py":
+            content  => template("${module_name}/tasks/halt.py.erb");
+    }
+}
