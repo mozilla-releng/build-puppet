5a9a5287dac9dcc817807f0adfb5e137fd2c06bb
Morgan Phillips <winter2718@gmail.com>
2014-12-08 19:55:23 +0000

    Bug 1103123 - Update cleanslate and runner to latest versions; r=rail
    --HG--
    extra : amend_source : bf698a996b0d75b4d7f37cbf816df0abec664b7a

--- modules/runner/manifests/init.pp
+++ modules/runner/manifests/init.pp
@@ -14,7 +14,7 @@ class runner {
             python   => $packages::mozilla::python27::python,
             require  => Class['packages::mozilla::python27'],
             packages => [
-                'runner==1.3',
+                'runner==1.4',
             ];
     }
 
