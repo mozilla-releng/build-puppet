a4c70a5dad8b239cd52b5113ca02716df0128d2b
Dustin J. Mitchell <dustin@mozilla.com>
2014-12-11 15:21:33 -0500

    Bug 1110311: upgrade and patch git-remote-hg; r=markco

--- modules/packages/manifests/mozilla/git_remote_hg.pp
+++ modules/packages/manifests/mozilla/git_remote_hg.pp
@@ -8,7 +8,7 @@ class packages::mozilla::git_remote_hg {
         CentOS: {
             package {
                 "git-remote-hg":
-                    ensure => 'fdac7e0-1.el6';
+                    ensure => '185852e-1.el6';
             }
         }
         default: {
