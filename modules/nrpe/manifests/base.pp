7b7be7000f46790aeaa6ff5392b1c1b384898fe0
Mark Cornmesser <mcornmesser@mozilla.com>
2015-01-14 12:31:31 -0800

    BUG1097783 NRPE module support for NSCLient++ r=dustin

--- modules/nrpe/manifests/base.pp
+++ modules/nrpe/manifests/base.pp
@@ -4,32 +4,49 @@
 class nrpe::base {
     include nrpe::settings
     include nrpe::service
-    include packages::nrpe
     include config # for vars for templates
-
+    
     $plugins_dir = $nrpe::settings::plugins_dir
     $nrpe_etcdir = $nrpe::settings::nrpe_etcdir
 
-    # configure
-    file {
-        "${nrpe_etcdir}":
-            ensure => directory,
-            owner => $::users::root::username,
-            group => $::users::root::group,
-            require => Class["packages::nrpe"];
-        "${nrpe_etcdir}/nrpe.cfg":
-            content => template("nrpe/nrpe.cfg.erb"),
-            owner => $::users::root::username,
-            group => $::users::root::group,
-            require => Class["packages::nrpe"],
-            notify => Class['nrpe::service'];
-        "${nrpe_etcdir}/nrpe.d":
-            ensure => directory,
-            owner => $::users::root::username,
-            group => $::users::root::group,
-            recurse => true,
-            purge => true,
-            require => Class["packages::nrpe"],
-            notify => Class['nrpe::service'];
+    case $::operatingsystem {
+        # On Windows NSClient++ is used in lieu of nrpe
+        # Configuration of NSCLient++ is handled in the nsc.ini file
+        Windows: {
+            include packages::nsclient_plus_plus
+            concat { 'c:\program files\nsclient++\nsc.ini':
+                require => Class["packages::nsclient_plus_plus"],
+            }
+            concat::fragment { 'nsc.ini.header':
+                target   => 'c:\program files\nsclient++\nsc.ini',
+                content  => template("nrpe/nsc.ini.erb"),
+                order    => '01',
+            }
+        }
+        # configure
+        default: {
+            include packages::nrpe
+            file {
+                "${nrpe_etcdir}":
+                    ensure => directory,
+                    owner => $::users::root::username,
+                    group => $::users::root::group,
+                    require => Class["packages::nrpe"];
+                "${nrpe_etcdir}/nrpe.cfg":
+                    content => template("nrpe/nrpe.cfg.erb"),
+                    owner => $::users::root::username,
+                    group => $::users::root::group,
+                    require => Class["packages::nrpe"],
+                    notify => Class['nrpe::service'];
+                "${nrpe_etcdir}/nrpe.d":
+                    ensure => directory,
+                    owner => $::users::root::username,
+                    group => $::users::root::group,
+                    recurse => true,
+                    purge => true,
+                    require => Class["packages::nrpe"],
+                    notify => Class['nrpe::service'];
+            }
+        }
     }
 }
