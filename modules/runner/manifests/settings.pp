3c4f8b2dae63469973fc16cafe1e1a149131ff68
Morgan Phillips <winter2718@gmail.com>
2015-01-30 19:45:22 +0000

    Bug 1127521 - Hard code runner's required path into puppet; r=dustin
    It was not obvious that <%=@path=> literally corresponded to the PATH environment variable of the puppet user.
    
    --HG--
    extra : rebase_source : 60b8336e31cd88c8a4c2e2dbe4c93f730247d1b1

--- modules/runner/manifests/settings.pp
+++ modules/runner/manifests/settings.pp
@@ -4,14 +4,17 @@
 # settings for runner
 class runner::settings {
     case $::operatingsystem {
-        'CentOS', 'Ubuntu', 'Darwin': {
-            $root = '/opt/runner'
+        'CentOS', 'Ubuntu': {
+            $runner_path = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
         }
-
+        'Darwin': {
+            $runner_path = "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/X11"
+        }
         default: {
             fail("Unsupported OS ${::operatingsystem}")
         }
     }
+    $root = '/opt/runner'
     $taskdir = "${root}/tasks.d"
     $configdir = "${root}/config.d"
     $task_hook = "${root}/task_hook.py"
