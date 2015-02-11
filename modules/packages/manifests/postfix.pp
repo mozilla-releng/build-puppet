d4ee3ea85a31623184542c4cf7c90fe88bac9a48
Dustin J. Mitchell <dustin@mozilla.com>
2015-02-11 11:46:43 -0800

    Bug 1130584: remove ssmtp and install postfix; r=dividehex

--- modules/packages/manifests/postfix.pp
+++ modules/packages/manifests/postfix.pp
@@ -10,18 +10,35 @@ class packages::postfix {
             }
         }
         CentOS: {
-            package {
-                "postfix":
-                    ensure => latest;
-                "ssmtp":
-                    notify => Exec["update-mta-alternatives"],
-                    ensure => absent,
-                    before => Package["postfix"];
-            }
+            # on CentOS, ssmtp and postfix both satisfy the same virtual, but
+            # ssmtp tends to get pulled in for other things like cronie or
+            # nagios-plugins.  Unfortunately, that means we can't uninstall
+            # ssmtp cleanly before installing postfix with the regular puppet
+            # `package` provider.  So we use a little bit of 'yum shell' to
+            # switch out ssmtp and postfix in a single transaction, if ssmtp is
+            # installed.  It's possible to have both installed at once, with
+            # an alternates invocation to point things in the right direction,
+            # but it's safer to just ensure ssmtp is not installed.
+
+            file {
+                '/root/switch-ssmtp-postfix':
+                    content => "erase ssmtp\ninstall postfix\nrun\n";
+            } ->
+            exec {
+                'switch-ssmtp-postfix':
+                    onlyif => '/bin/rpm -qi ssmtp',
+                    command => '/usr/bin/yum -y shell < /root/switch-ssmtp-postfix';
+            } ~>
             exec {
                 "update-mta-alternatives":
                     command     => "/usr/sbin/alternatives --auto mta",
                     refreshonly => true;
+            }
+
+            # let Puppet take care of updating
+            package {
+                "postfix":
+                    ensure => latest;
             }
         }
 
