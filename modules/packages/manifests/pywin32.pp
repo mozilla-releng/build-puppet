d5a224abba0608616dff056ab823ed49de17b4a1
Mark Cornmesser <mcornmesser@mozilla.com>
2015-02-27 14:54:34 -0800

    BUG1134980 Use python easy_install meathod to install pywin32 r=dustin

--- modules/packages/manifests/pywin32.pp
+++ modules/packages/manifests/pywin32.pp
@@ -3,17 +3,12 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 # pywin32-218.win32-py2.7.exe  original package available at http://sourceforge.net/projects/pywin32/files/pywin32/Build%20218/
-# This package has been manual repackage using 7zip.
-# The original exe file did not accept cmd line arguments for installation.
-# The exe was extracting and re-zipped into the 2 files below. 
-# Removing parent directories and allowing the packages being extracted to the proper directories. 
 class packages::pywin32 {
-    packages::pkgzip {"pywin32-218.win32-py2.7-LIB.zip":
-        zip => "pywin32-218.win32-py2.7-LIB.zip",
-        target_dir => 'C:\mozilla-build\python\lib\site-packages';
-    }
-    packages::pkgzip {"pywin32-218.win32-py2.7-SCRIPTS.zip":
-        zip => "pywin32-218.win32-py2.7-SCRIPTS.zip",
-        target_dir => 'C:\mozilla-build\python\scripts';
+    include packages::mozilla::python27
+
+    exec { "pywin32":
+        require => Class['packages::mozilla::python27'],
+        command => 'C:\mozilla-build\python\Scripts\easy_install.exe  http://puppetagain.pub.build.mozilla.org/data/repos/EXEs/pywin32-218.win32-py2.7.exe',
+        creates => 'C:\mozilla-build\\Lib\\site-packages\\pywin32-218-py2.7-win32.egg';
     }
 }
