9f5b4d107ca7d492d34a3265888072befda9ad40
Jake Watkins <jwatkins@mozilla.com>
2015-03-26 17:24:48 -0700

    Bug 1141626 and 1141628: adds temp node defs; a=nodechange

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -858,3 +858,14 @@ node /log-aggregator\d+\.srv\.releng\.(scl3|use1|usw2)\.mozilla\.com/ {
 }
 
 ## Loaners
+
+## temporary hosts Bug 1141628 and 1141626
+
+node "bld-lion-r4-001.test.releng.scl3.mozilla.com" {
+    include toplevel::base
+}
+
+node "mac-v2-signing5.test.releng.scl3.mozilla.com" {
+    include toplevel::base
+}
+
