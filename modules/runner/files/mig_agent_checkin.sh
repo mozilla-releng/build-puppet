22b2f8f97af46acbce42202463e282eb42527174
Dustin J. Mitchell <dustin@mozilla.com>
2015-04-06 14:12:01 -0700

    Bug 1116210: allow mig runner task to sudo to root on os x; r=ulfr

--- modules/runner/files/mig_agent_checkin.sh
+++ modules/runner/files/mig_agent_checkin.sh
@@ -3,5 +3,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# use sudo when we aren't running as root (i.e., on OS X)
+if [ "$UID" != 0 ]; then
+    PREFIX="sudo"
+fi
+
 # run mig-agent in checkin mode
-/sbin/mig-agent -m agent-checkin || true
+$PREFIX /sbin/mig-agent -m agent-checkin || true
