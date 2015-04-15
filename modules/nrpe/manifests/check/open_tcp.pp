06f32f5be88e7e49994b25f05418229144b1a97d
Amy Rich <arich@mozilla.com>
2015-04-15 13:29:39 -0400

    bug 1140003: add nagios check for number of open tcp sockets; r=dustin

---
+++ modules/nrpe/manifests/check/open_tcp.pp
@@ -0,0 +1,22 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+class nrpe::check::open_tcp {
+    include nrpe::settings
+    $plugins_dir = $nrpe::settings::plugins_dir
+
+    nrpe::check {
+        'check_open_tcp':
+            cfg => "$plugins_dir/check_open_tcp -w \$ARG1\$ -c \$ARG2\$ -p \$ARG3\$";
+    }
+
+    nrpe::plugin {
+        'check_open_tcp': ;
+    }
+
+    sudoers::custom {
+        'check_open_tcp':
+            user => 'nagios',
+            command => "/usr/sbin/lsof";
+    }
+}
