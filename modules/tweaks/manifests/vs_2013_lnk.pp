3217a00379c4126b5ace8f710217316ac4757a6b
Mark Cornmesser <mcornmesser@mozilla.com>
2015-05-28 12:54:55 -0700

    Bug 1169325 Have CRTRES.EXE available for Visual Studio 2010 on 2008 slaves r=dustin

--- modules/tweaks/manifests/vs_2013_lnk.pp
+++ modules/tweaks/manifests/vs_2013_lnk.pp
@@ -10,5 +10,18 @@ class tweaks::vs_2013_lnk {
             ensure => link, 
             target => 'C:\Program Files (x86)\Microsoft Visual Studio 12.0';
     }
+    # cvtres.exe is originally from the Microsoft 2013 Visual Studio install
+    # currently we need to be available to VS 2010 
+    file {
+        'C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin/cvtres.exe' :
+            ensure => file,
+            source => "puppet:///repos/private/EXEs/cvtres.exe"; 
+    }
+    file {
+        'C:/Program Files (x86)/Microsoft Visual Studio 10.0/VC/bin/amd64/cvtres.exe' :
+            ensure => file,
+            source => "puppet:///repos/private/EXEs/cvtres.exe";
+    }
+
 }
 
