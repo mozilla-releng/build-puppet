77180d55f30439538b8003533f208bb1697356f0
Rail Aliiev <rail@mozilla.com>
2015-06-12 08:38:46 -0400

    bug 1173960: missing package in funsize scheduler deployment. r=bhearsum

--- modules/funsize_scheduler/manifests/init.pp
+++ modules/funsize_scheduler/manifests/init.pp
@@ -29,6 +29,7 @@ class funsize_scheduler {
                 "funsize==0.6",
                 "Jinja2==2.7.1",
                 "kombu==3.0.26",
+                "MarkupSafe==0.23",
                 "PGPy==0.3.0",
                 "pycparser==2.13",
                 "PyHawk-with-a-single-extra-commit==0.1.5",
