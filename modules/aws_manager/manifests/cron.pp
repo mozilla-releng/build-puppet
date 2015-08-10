a4b5295a28a76151e3bba2e32a40c68543969c50
Rob Thijssen <rthijssen@mozilla.com>
2015-08-10 12:45:58 +0100

    Bug 1192760: Add windows golden ami generators to cron; r=arr

--- modules/aws_manager/manifests/cron.pp
+++ modules/aws_manager/manifests/cron.pp
@@ -108,6 +108,24 @@ class aws_manager::cron {
             virtualenv_dir => "${aws_manager::settings::root}",
             user           => "${users::buildduty::username}",
             params         => "-c ${repo_root}/configs/tst-linux32 -r us-east-1 -s aws-releng -k ${aws_manager::settings::secrets_dir}/aws-secrets.json --ssh-key ${users::buildduty::home}/.ssh/aws-ssh-key -i ${repo_root}/instance_data/us-east-1.instance_data_tests.json --create-ami --ignore-subnet-check --copy-to-region us-west-2 tst-linux32-ec2-golden";
+        "y-2008-ec2-golden":
+            script         => "aws_create_instance.py",
+            ensure         => $cron_switch,
+            minute         => '30',
+            hour           => '1',
+            cwd            => "${aws_manager::settings::cloud_tools_dst}/scripts",
+            virtualenv_dir => "${aws_manager::settings::root}",
+            user           => "${users::buildduty::username}",
+            params         => "-c ${repo_root}/configs/y-2008 -r us-east-1 -s aws-releng -k ${aws_manager::settings::secrets_dir}/aws-secrets.json --ssh-key ${users::buildduty::home}/.ssh/aws-ssh-key -i ${repo_root}/instance_data/us-east-1.instance_data_try.json --create-ami --ignore-subnet-check --copy-to-region us-west-2 y-2008-ec2-golden";
+        "b-2008-ec2-golden":
+            script         => "aws_create_instance.py",
+            ensure         => $cron_switch,
+            minute         => '35',
+            hour           => '1',
+            cwd            => "${aws_manager::settings::cloud_tools_dst}/scripts",
+            virtualenv_dir => "${aws_manager::settings::root}",
+            user           => "${users::buildduty::username}",
+            params         => "-c ${repo_root}/configs/b-2008 -r us-east-1 -s aws-releng -k ${aws_manager::settings::secrets_dir}/aws-secrets.json --ssh-key ${users::buildduty::home}/.ssh/aws-ssh-key -i ${repo_root}/instance_data/us-east-1.instance_data_prod.json --create-ami --ignore-subnet-check --copy-to-region us-west-2 b-2008-ec2-golden";
         "tst-emulator64-ec2-golden":
             script         => "aws_create_instance.py",
             ensure         => $cron_switch,
