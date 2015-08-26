28abc467b506ebbfac572a4c5f5b5226706d6bae
Rail Aliiev <rail@mozilla.com>
2015-08-26 17:13:26 -0400

    Bug 1145774 - add av-linux64 slave type. r=bustage

--- modules/toplevel/manifests/slave/releng/build/mock/av.pp
+++ modules/toplevel/manifests/slave/releng/build/mock/av.pp
@@ -2,7 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-class toplevel::slave::releng::build::av inherits toplevel::slave::releng::build::mock {
+class toplevel::slave::releng::build::mock::av inherits toplevel::slave::releng::build::mock {
     include clamav::daemon
     include clamav::freshclam
 }
