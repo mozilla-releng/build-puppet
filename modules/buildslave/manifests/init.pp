5cecfc4c1b9703cc4f5c7084860f5a067e9d1908
Dustin J. Mitchell <dustin@mozilla.com>
2015-09-01 13:40:25 -0700

    Bug 1198779: only install one version of Buildbot; r=markco
    --HG--
    extra : commitid : Qe6feXW78V
    extra : rebase_source : 5f7f19fd6e568d49ff5b35326b5cdf217012ef2f
    extra : amend_source : fe77dc3d0f7b19f23aa4c984984cbb89b48d9491

--- modules/buildslave/manifests/init.pp
+++ modules/buildslave/manifests/init.pp
@@ -4,10 +4,7 @@
 # This module is responsible for installing and running buildbot-slave on all
 # slaves.  Do not be confused by the name 'buildslave' - this applies to all
 # slaves used in releng -- build, test, whatever.
-#
-# See:
-#  - https://wiki.mozilla.org/ReleaseEngineering/Buildslave_Startup_Process
-#  - https://wiki.mozilla.org/ReleaseEngineering/Buildslave_Versions
+
 class buildslave {
     anchor {
         'buildslave::begin': ;
