9cc6fd40c9eb76a2132ec9b555a88a9d138cd6d5
Dustin J. Mitchell <dustin@mozilla.com>
2015-09-09 11:53:14 -0400

    Bug 1198779: just install one copy of buildbot, in one place; r?markco
    --HG--
    extra : commitid : CsqHTfVTOK8
    extra : rebase_source : 37995b3ea2b5ad515213726a16ad2e34f352016d

--- modules/buildslave/manifests/install.pp
+++ modules/buildslave/manifests/install.pp
@@ -1,7 +1,6 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-# this class simply invokes the resource type with the production version
 class buildslave::install {
     include packages::mozilla::python27
 
@@ -10,11 +9,11 @@ class buildslave::install {
         'buildslave::install::end': ;
     }
 
-    $version = "0.8.4-pre-moz7" # N.B. used by runner
+    $version = "0.8.4-pre-moz7"
 
     $virtualenv_path = $::operatingsystem ? {
-        windows => "c:\\mozilla-build\\buildbot-$version",
-        default => "/tools/buildbot-$version",
+        windows => "c:\\mozilla-build\\buildbotve",
+        default => "/tools/buildbot",
     }
 
     $py_require = Class['packages::mozilla::python27']
@@ -34,26 +33,4 @@ class buildslave::install {
             require => $py_require,
             packages => $packages;
     } -> Anchor["buildslave::install::end"]
-
-    case $::operatingsystem {
-        Windows: {
-            file {
-                "C:/mozilla-build/bbpath.bat":
-                    content => "set BUILDBOT_PATH=$virtualenv_path";
-            }
-            # Append the virtual environment directory to the Windows path
-            windows_path {
-                "$virtualenv_path":
-                    ensure => present;
-            }
-        }
-        default: {
-            Anchor["buildslave::install::begin"] ->
-            file {
-                "/tools/buildbot":
-                    ensure => "link",
-                    target => "$virtualenv_path";
-            } -> Anchor["buildslave::install::end"]
-        }
-    }
 }
