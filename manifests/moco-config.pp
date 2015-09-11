4c16973d2fc4216245f0a16055689c5b1d2a1214
Ben Hearsum <bhearsum@mozilla.com>
2015-09-11 16:40:59 -0400

    Bump buildbot bridge dev version to pick up bug 1196407. a=trivial

--- manifests/moco-config.pp
+++ manifests/moco-config.pp
@@ -340,7 +340,7 @@ class config inherits config::base {
 
     $buildbot_bridge_env_config = {
         "dev" => {
-            version => "1.5",
+            version => "1.5.1",
             client_id => secret("buildbot_bridge_dev_taskcluster_client_id"),
             access_token => secret("buildbot_bridge_dev_taskcluster_access_token"),
             dburi => secret("buildbot_bridge_dev_dburi"),
