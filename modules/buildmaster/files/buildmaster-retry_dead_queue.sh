d7cef5e597bd679e05b03229cce0a09660977804
kmoir@mozilla.com <kmoir@mozilla.com>
2015-09-30 10:37:28 -0400

    Bug 1158729 - manage_masters.py retry_dead_queue should run periodically r=kmoir patch by vladC

--- modules/buildmaster/files/buildmaster-retry_dead_queue.sh
+++ modules/buildmaster/files/buildmaster-retry_dead_queue.sh
@@ -5,9 +5,15 @@ do
     for f in /dev/shm/queue/${i}/dead/*
     do
         find /dev/shm/queue/${i}/dead/ -iname "*.log" -delete
-        if [[ -e ${f} ]]; 
+        if ! [ $? -eq 0 ]; then
+            echo "ERROR: Unable to remove the log files on $(hostname)"
+        fi
+        if [[ -e ${f} ]];
         then
             mv  ${f} /dev/shm/queue/${i}/new/
+        if ! [ $? -eq 0 ]; then
+            echo "ERROR: The following job ${f} has not been moved from the dead directory in the new directory on $(hostname)"
+        fi
         else
         continue
     fi
