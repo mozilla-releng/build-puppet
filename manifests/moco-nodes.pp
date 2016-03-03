ce7d3c4934057a2441325224879a0e117ce2a960
Vlad Ciobancai <vlad.ciobancai@softvision.ro>
2016-03-03 18:50:53 +0200

    Bug 1252248 - Add more tst-linux64, tst-emulator64 capacity in AWS, r=kmoir

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -1052,6 +1052,28 @@ node "buildbot-master129.bb.releng.usw2.mozilla.com" {
     include toplevel::server::buildmaster::mozilla
 }
 
+node "buildbot-master130.bb.releng.use1.mozilla.com" {
+    $node_security_level = 'high'
+    buildmaster::buildbot_master::mozilla {
+        "bm130-tests1-linux64":
+            http_port => 8201,
+            master_type => "tests",
+            basedir => "tests1-linux64";
+    }
+    include toplevel::server::buildmaster::mozilla
+}
+
+node "buildbot-master131.bb.releng.usw2.mozilla.com" {
+    $node_security_level = 'high'
+    buildmaster::buildbot_master::mozilla {
+        "bm131-tests1-linux64":
+            http_port => 8201,
+            master_type => "tests",
+            basedir => "tests1-linux64";
+    }
+    include toplevel::server::buildmaster::mozilla
+}
+
 node /log-aggregator\d+\.srv\.releng\.(scl3|use1|usw2)\.mozilla\.com/ {
     $is_log_aggregator_host = "true"
     include toplevel::server::log_aggregator
