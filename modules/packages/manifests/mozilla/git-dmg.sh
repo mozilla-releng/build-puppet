56cb9ca8a84cc167f281c86447b3bc459574126a
Jake Watkins <jwatkins@mozilla.com>
2016-03-23 08:32:53 -0700

    Bug 1257633: upgrade git to 2.7.4 on osx; r=dustin

--- modules/packages/manifests/mozilla/git-dmg.sh
+++ modules/packages/manifests/mozilla/git-dmg.sh
@@ -2,16 +2,11 @@
 
 set -e
 
-if ! test -f git.spec; then
-    echo "Run this from the root of the unpacked SRPM (it uses the sources)"
-    exit 1
-fi
-
-# variables to parallel the spec file
 realname=git
-version=1.7.9.4
+version=2.7.4
 release=1
 _prefix=/tools/$realname
+URL="https://www.kernel.org/pub/software/scm/git/git-${version}.tar.gz"
 
 # set up a clean build dir
 if test -d build; then
@@ -21,8 +16,10 @@ mkdir build
 BUILD=$PWD/build
 cd $BUILD
 
+curl -LO $URL
+
 # %prep
-tar -zxf ../$realname-$version.tar.gz
+tar -zxf $realname-$version.tar.gz
 cd $realname-$version
 
 # %build
@@ -37,11 +34,11 @@ ln -s $_prefix/bin/git $ROOT/usr/local/bin/git
 
 # build the package (no RPM equivalent, sorry)
 cd $BUILD
-mkdir dmg 
+mkdir dmg
 fullname=$realname-$version-$release
 pkg=dmg/$fullname.pkg
 dmg=$fullname.dmg
-/Developer/usr/bin/packagemaker -r $ROOT -v -i com.mozilla.$realname -o $pkg -l /
+pkgbuild --root $ROOT --identifier com.mozilla.$realname  --install-location / $pkg
 hdiutil makehybrid -hfs -hfs-volume-name "mozilla-$realname-$version-$release" -o ./$dmg dmg
 echo "Result:"
 echo $PWD/$dmg
