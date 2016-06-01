ef9220483179d1396b86b9be13c65196e5fc6630
Mark Cornmesser <mcornmesser@mozilla.com>
2016-06-01 09:57:24 -0700

    Bug 1241226 - Win 7 Install Nvidia Drivers r=divdiehex

--- modules/packages/manifests/nvidia_drivers.pp
+++ modules/packages/manifests/nvidia_drivers.pp
@@ -5,7 +5,9 @@
 class packages::nvidia_drivers {
     include needs_reboot
 
-    realize(Packages::Aptrepo['xorg-edgers'])
+    if ($::operatingsystem != Windows) {
+        realize(Packages::Aptrepo['xorg-edgers'])
+    }
 
     # The Ubuntu xorg-edgers reqo embeds the version number in the package
     # name, so we can easily require "latest"
@@ -34,6 +36,64 @@ class packages::nvidia_drivers {
             }
         }
 
+        Windows: {
+            # This should serve as the basic frame work for the nvidia driver install on testers in the datacenter and AWS
+            # However, the install has only been tested on Win 7 in AWS
+            # Further testing will be needed in the other environments
+
+            $datacnter_domain = "build.mozilla.org"
+            $nv_dir           = "C:\\installersource\\puppetagain.pub.build.mozilla.org\\ZIPs\\nvidia\\"
+
+            $arch = $hardwaremodel ? {
+                i686    => "32bit",
+                default => "64bit",
+            }
+            # The default domain should cover testers in use1 and usw2
+            $nv_version = $domain ? {
+                "$datacnter_domain" => "314.07",
+                default             => "354.42",
+            }
+            $nv_name = $domain ? {
+                "$datacnter_domain" => "-desktop-win8-win7-winvista-",
+                default             => "-quadro-grid-desktop-notebook-win8-win7-",
+            }
+            $lang = $domain ? {
+                "$datacnter_domain" => "-english-whql",
+                 default            => "-international-whql",
+            }
+            # The installation in AWS exits with 1 even though the driver is installed and functioning
+            # The exit code is due to a dll file failing to be unloaded
+            $ex_code = $domain ? {
+                "$datacnter_domain" => "0",
+                default             => "0, 1",
+            }
+
+            $nv_driver = "$nv_version$nv_name$arch$lang"
+
+            packages::pkgzip {
+                "nv_driver":
+                    zip        => "$nv_driver.zip",
+                    private    => true,
+                    target_dir => "c:\\InstallerSource\\puppetagain.pub.build.mozilla.org\\zips\\nvidia";
+            }
+            # This will fail on any AWS instance type that is not g2
+            exec {
+                "nvidia_setup_exe":
+                    command     => "$nv_dir$nv_driver\\setup.exe -clean -passive -noreboot -loglevel:6 -log:C:\\ProgramData\\PuppetLabs\\puppet\\var\\log",
+                    returns     => "$ex_code",
+                    subscribe   => Packages::Pkgzip["nv_driver"],
+                    refreshonly => true;
+            }
+            # No need to keep these files around
+            file {
+                "C:/installersource/puppetagain.pub.build.mozilla.org/ZIPs/nvidia-$nvidia_full_version-${arch}bit":
+                    ensure  => absent,
+                    purge   => true,
+                    recurse => true,
+                    force   => true,
+                    require => Exec["nvidia_setup_exe"];
+            }
+        }
         default: {
             fail("cannot install on $::operatingsystem")
         }
