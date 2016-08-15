4d8a6b82e57ae6f6ba38732d83544b2c629a66e7
Jake Watkins <jwatkins@mozilla.com>
2016-08-15 09:26:10 -0700

    Bug 1294125: change net.ipv4.tcp_challenge_ack_limit

---
+++ modules/tweaks/manifests/tcp_challenge_ack_limit.pp
@@ -0,0 +1,27 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# Limits number of Challenge ACK sent per second, as recommended
+# in RFC 5961 (Improving TCP's Robustness to Blind In-Window Attacks)
+
+# This is suppose to be a temporary patch to prevent Linux TCP ChallengeAck
+# side channel attack.  Although it will be permanent in the releng use case
+# since upgrading all kernels to >= 4.7 isn't feasible.
+
+# See https://bugzilla.mozilla.org/show_bug.cgi?id=1294125
+
+class tweaks::tcp_challenge_ack_limit {
+    case $::operatingsystem {
+        CentOS, Ubuntu: {
+            sysctl::value {
+                'net.ipv4.tcp_challenge_ack_limit':
+                    value => 999999999;
+            }
+        }
+        default: {
+            notice("Don't know how to set tcp_challenge_ack_limit on $::operatingsystem")
+        }
+    }
+}
+
