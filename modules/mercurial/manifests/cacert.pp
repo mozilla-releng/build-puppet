305895d476d22016154a44520da861089b45e5cb
Amy Rich <arich@mozilla.com>
2016-09-21 13:56:34 -0400

    bug 8793369: add configuration to install new cacert.pem into /etc/mercurial/cacert/cacert.pem; r=gps

---
+++ modules/mercurial/manifests/cacert.pp
@@ -0,0 +1,23 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+# install cacert.pem and place it in /etc/mercurial/ (Ubuntu, CentOS and Darwin)
+
+class mercurial::cacert {
+    case $operatingsystem {
+        CentOS,Ubuntu,Darwin: {
+            file {
+                '/etc/mercurial/cacert':
+                    ensure => directory,
+                    mode => 0755;
+                
+                '/etc/mercurial/cacert/cacert.pem':
+                    ensure => file,
+                    source => "puppet:///modules/mercurial/cacert.pem",
+                    owner => "root";
+            }
+        }
+    }
+}
+
