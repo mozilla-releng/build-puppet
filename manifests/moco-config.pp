b88a086485bfa46b245ea535a57e1a6bde0ef517
Johan Lorenzo <jlorenzo@mozilla.com>
2016-10-03 14:49:58 +0200

    Bug 1307826 - Deploy PushApkWorker on its own production machine
    MozReview-Commit-ID: 5FKYwcxZSJL
    
    --HG--
    extra : rebase_source : e6e579f1bcb74ffa4c63e557de4a0972a9d0f4d4

--- manifests/moco-config.pp
+++ manifests/moco-config.pp
@@ -448,6 +448,73 @@ class config inherits config::base {
         }
     }
 
+    ## TC pushapk scriptworkers
+    $pushapk_scriptworker_root = '/builds/pushapkworker'
+    $pushapk_scriptworker_worker_config = "${pushapk_scriptworker_root}/config.json"
+    $pushapk_scriptworker_script_config = "${pushapk_scriptworker_root}/script_config.json"
+
+    $pushapk_scriptworker_jarsigner_keystore = "${pushapk_scriptworker_root}/mozilla-android-keystore"
+    $pushapk_scriptworker_jarsigner_nightly_certificate_alias = 'nightly'
+    $pushapk_scriptworker_jarsigner_release_certificate_alias = 'release'
+    $pushapk_scriptworker_taskcluster_artifact_expiration_hours = 336
+    $pushapk_scriptworker_taskcluster_artifact_upload_timeout = 1200
+    $pushapk_scriptworker_task_max_timeout = 1200
+    $pushapk_scriptworker_artifact_expiration_hours = 336
+    $pushapk_scriptworker_artifact_upload_timeout = 600
+    $pushapk_scriptworker_env_config = {
+      'dev' => {
+        provisioner_id => 'scriptworker-prov-v1',
+        worker_group => 'pushapk-v1-dev',
+        worker_type => 'pushapk-v1-dev',
+        worker_id => 'jlorenzo-dev',
+        verbose_logging => true,
+        taskcluster_client_id => secret('pushapk_scriptworker_taskcluster_client_id_dev'),
+        taskcluster_access_token => secret('pushapk_scriptworker_taskcluster_access_token_dev'),
+        google_play_config => {
+          'aurora' => {
+            service_account => secret('pushapk_scriptworker_aurora_google_play_service_account_dev'),
+            certificate => secret('pushapk_scriptworker_aurora_google_play_certificate_dev'),
+            certificate_target_location => "${pushapk_scriptworker_root}/aurora.p12",
+          },
+          'beta' => {
+            service_account => secret('pushapk_scriptworker_beta_google_play_service_account_dev'),
+            certificate => secret('pushapk_scriptworker_beta_google_play_certificate_dev'),
+            certificate_target_location => "${pushapk_scriptworker_root}/beta.p12",
+          },
+          'release' => {
+            service_account => secret('pushapk_scriptworker_release_google_play_service_account_dev'),
+            certificate => secret('pushapk_scriptworker_release_google_play_certificate_dev'),
+            certificate_target_location => "${pushapk_scriptworker_root}/release.p12",
+          },
+        },
+      },
+      'prod' => {
+        provisioner_id => 'scriptworker-prov-v1',
+        worker_group => 'pushapk-v1',
+        worker_type => 'pushapk-v1',
+        verbose_logging => true,
+        taskcluster_client_id => secret('pushapk_scriptworker_taskcluster_client_id_prod'),
+        taskcluster_access_token => secret('pushapk_scriptworker_taskcluster_access_token_prod'),
+        google_play_config => {
+          'aurora' => {
+            service_account => secret('pushapk_scriptworker_aurora_google_play_service_account_prod'),
+            certificate => secret('pushapk_scriptworker_aurora_google_play_certificate_prod'),
+            certificate_target_location => "${pushapk_scriptworker_root}/aurora.p12",
+          },
+          'beta' => {
+            service_account => secret('pushapk_scriptworker_beta_google_play_service_account_prod'),
+            certificate => secret('pushapk_scriptworker_beta_google_play_certificate_prod'),
+            certificate_target_location => "${pushapk_scriptworker_root}/beta.p12",
+          },
+          'release' => {
+            service_account => secret('pushapk_scriptworker_release_google_play_service_account_prod'),
+            certificate => secret('pushapk_scriptworker_release_google_play_certificate_prod'),
+            certificate_target_location => "${pushapk_scriptworker_root}/release.p12",
+          },
+        },
+      },
+    }
+
     # Funsize Scheduler configuration
     $funsize_scheduler_root = "/builds/funsize"
     $funsize_scheduler_balrog_username = "funsize"
---
+++ modules/java_ks/lib/puppet/type/java_ks.rb
@@ -0,0 +1,187 @@
+Puppet::Type.newtype(:java_ks) do
+  @doc = 'Manages the entries in a java keystore, and uses composite namevars to
+  accomplish the same alias spread across multiple target keystores.'
+
+  ensurable do
+
+    desc 'Has three states: present, absent, and latest.  Latest
+      will compare the on disk MD5 fingerprint of the certificate to that
+      in keytool to determine if insync? returns true or false.  We redefine
+      insync? for this paramerter to accomplish this.'
+
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    newvalue(:latest) do
+      if provider.exists?
+        provider.update
+      else
+        provider.create
+      end
+    end
+
+    def insync?(is)
+
+      @should.each do |should|
+        case should
+        when :present
+          return true if is == :present
+        when :absent
+          return true if is == :absent
+        when :latest
+          unless is == :absent
+            return true if provider.latest == provider.current
+          end
+        end
+      end
+
+      return false
+    end
+
+    defaultto :present
+  end
+
+  newparam(:name) do
+    desc 'The alias that is used to identify the entry in the keystore. This will be
+    converted to lowercase.'
+
+    isnamevar
+
+    munge do |value|
+      value.downcase
+    end
+  end
+
+  newparam(:target) do
+    desc 'Destination file for the keystore.  This will autorequire the parent directory of the file.'
+
+    isnamevar
+  end
+
+  newparam(:certificate) do
+    desc 'A server certificate, followed by zero or more intermediate certificate authorities.
+      All certificates will be placed in the keystore.  This will autorequire the specified file.'
+
+    isrequired
+  end
+
+  newparam(:storetype) do
+    desc 'Optional storetype
+      Valid options: <jceks>'
+
+    newvalues(:jceks)
+  end
+
+  newparam(:private_key) do
+    desc 'If you want an application to be a server and encrypt traffic,
+      you will need a private key.  Private key entries in a keystore must be
+      accompanied by a signed certificate for the keytool provider. This will autorequire the specified file.'
+  end
+
+  newparam(:chain) do
+    desc 'The intermediate certificate authorities, if they are to be taken
+      from a file separate from the server certificate. This will autorequire the specified file.'
+  end
+
+  newparam(:password) do
+    desc 'The password used to protect the keystore.  If private keys are
+      subsequently also protected this password will be used to attempt
+      unlocking. Must be six or more characters in length. Cannot be used
+      together with :password_file, but you must pass at least one of these parameters.'
+
+    validate do |value|
+      raise Puppet::Error, "password is #{value.length} characters long; must be 6 characters or greater in length" if value.length < 6
+    end
+  end
+
+  newparam(:password_file) do
+    desc 'The path to a file containing the password used to protect the
+      keystore. This cannot be used together with :password, but you must pass at least one of these parameters.'
+  end
+
+  newparam(:destkeypass) do
+    desc 'The password used to protect the key in keystore.'
+
+    validate do |value|
+      raise Puppet::Error, "destkeypass is #{value.length} characters long; must be of length 6 or greater" if value.length < 6
+    end
+  end
+
+  newparam(:trustcacerts) do
+    desc "Certificate authorities aren't by default trusted so if you are adding a CA you need to set this to true.
+     Defaults to :false."
+
+    newvalues(:true, :false)
+
+    defaultto :false
+  end
+
+  newparam(:path) do
+    desc "The search path used for command (keytool, openssl) execution.
+      Paths can be specified as an array or as a '#{File::PATH_SEPARATOR}' separated list."
+
+    # Support both arrays and colon-separated fields.
+    def value=(*values)
+      @value = values.flatten.collect { |val|
+        val.split(File::PATH_SEPARATOR)
+      }.flatten
+    end
+  end
+
+  # Where we setup autorequires.
+  autorequire(:file) do
+    auto_requires = []
+    [:private_key, :certificate, :chain].each do |param|
+      if @parameters.include?(param)
+        auto_requires << @parameters[param].value
+      end
+    end
+    if @parameters.include?(:target)
+      auto_requires << ::File.dirname(@parameters[:target].value)
+    end
+    auto_requires
+  end
+
+  # Our title_patterns method for mapping titles to namevars for supporting
+  # composite namevars.
+  def self.title_patterns
+    identity = lambda {|x| x}
+    [
+      [
+        /^([^:]+)$/,
+        [
+          [ :name, identity ]
+        ]
+      ],
+      [
+        /^(.*):([a-z]:(\/|\\).*)$/i,
+        [
+            [ :name, identity ],
+            [ :target, identity ]
+        ]
+      ],
+      [
+        /^(.*):(.*)$/,
+        [
+          [ :name, identity ],
+          [ :target, identity ]
+        ]
+      ]
+    ]
+  end
+
+  validate do
+    if value(:password) and value(:password_file)
+      self.fail "You must pass either 'password' or 'password_file', not both."
+    end
+
+    unless value(:password) or value(:password_file)
+      self.fail "You must pass one of 'password' or 'password_file'."
+    end
+  end
+end
