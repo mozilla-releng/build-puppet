7f927016db3f55a55106954acf60b0e4a2db64b0
Mihai Tabara <mtabara@mozilla.com>
2016-11-08 02:08:32 +0000

    Bug 1313154 - bump beetmoverscript version in beetmoverworker. r=nthomas
    MozReview-Commit-ID: 2RBDCCKIrNA

--- modules/beetmover_scriptworker/manifests/init.pp
+++ modules/beetmover_scriptworker/manifests/init.pp
@@ -49,7 +49,7 @@ class beetmover_scriptworker {
                   "jmespath==0.9.0",
                   "PyYAML==3.12",
                   "Jinja2==2.8",
-                  "beetmoverscript==0.0.5",
+                  "beetmoverscript==0.0.6",
                   "MarkupSafe==0.23",
                   "s3transfer==0.1.8",
                   "docutils==0.12",
