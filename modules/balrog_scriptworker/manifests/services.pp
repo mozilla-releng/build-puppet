2fdf411410bc42a0fb282ce51d8761d0c3291a71
Mihai Tabara <mtabara@mozilla.com>
2016-11-11 01:03:50 +0200

    Bug 1314517 - Refactor balrogworker deployment to be more like beetmover. r=jlund
    MozReview-Commit-ID: 8HnR4KfsrAs

--- modules/balrog_scriptworker/manifests/services.pp
+++ modules/balrog_scriptworker/manifests/services.pp
@@ -5,16 +5,16 @@ class balrog_scriptworker::services {
 
     supervisord::supervise {
         "balrog_scriptworker":
-            command      => "${balrog_scriptworker::settings::py35venv}/bin/scriptworker ${balrog_scriptworker::settings::root}/config.json",
+            command      => "${balrog_scriptworker::settings::root}/bin/scriptworker ${balrog_scriptworker::settings::root}/config.json",
             user         => $::config::builder_username,
             require      => [ File["${balrog_scriptworker::settings::root}/config.json"]],
             extra_config => template("${module_name}/supervisor_config.erb");
     }
     exec {
-        "restart-scriptworker":
+        "restart-balrogworker":
             command     => "/usr/bin/supervisorctl restart balrog_scriptworker",
             refreshonly => true,
-            subscribe   => [Python35::Virtualenv["${balrog_scriptworker::settings::py35venv}"],
+            subscribe   => [Python35::Virtualenv["${balrog_scriptworker::settings::root}"],
                             File["${balrog_scriptworker::settings::root}/config.json"]];
     }
 }
