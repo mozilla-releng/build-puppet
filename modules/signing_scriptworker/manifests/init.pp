7c697b077ce9e7c9645c6db6c72a7635e03c0c52
Aki Sasaki <asasaki@mozilla.com>
2016-11-14 17:45:14 -0800

    bug 1309293 - scriptworker 1.0.0b1. r=callek

--- modules/signing_scriptworker/manifests/init.pp
+++ modules/signing_scriptworker/manifests/init.pp
@@ -40,7 +40,7 @@ class signing_scriptworker {
                   "python-jose==1.3.2",
                   "PyYAML==3.12",
                   "requests==2.11.1",
-                  "scriptworker==0.10.0a8",
+                  "scriptworker==1.0.0b1",
                   "signingscript==0.8.0",
                   "signtool==2.0.3",
                   "six==1.10.0",
