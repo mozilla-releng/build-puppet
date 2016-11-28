21edbdf1aab82609cbdc024c78ee86dc4857181c
Aki Sasaki <asasaki@mozilla.com>
2016-11-28 13:57:38 -0800

    bug 1319186 - signingscript 0.8.2. r=callek

--- modules/signing_scriptworker/manifests/init.pp
+++ modules/signing_scriptworker/manifests/init.pp
@@ -41,7 +41,7 @@ class signing_scriptworker {
                   "PyYAML==3.12",
                   "requests==2.11.1",
                   "scriptworker==1.0.0b2",
-                  "signingscript==0.8.1",
+                  "signingscript==0.8.2",
                   "signtool==2.0.3",
                   "six==1.10.0",
                   "slugid==1.0.7",
