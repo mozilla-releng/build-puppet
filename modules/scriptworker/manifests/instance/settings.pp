dda8c4e5b23db8ece9913a0672ab60a34ce6dcfe
Mihai Tabara <mtabara@mozilla.com>
2017-01-04 15:11:06 +0200

    Fix provisioner by feeding a separte temp var in scriptworker.

--- modules/scriptworker/manifests/instance/settings.pp
+++ modules/scriptworker/manifests/instance/settings.pp
@@ -4,7 +4,7 @@
 # (private)
 class scriptworker::instance::settings {
     $git_key_repo_url = "https://github.com/mozilla-releng/cot-gpg-keys.git"
-    $provisioner_id = "test-dummy-provisioner"
+    $provisioner_id = "scriptworker-prov-v1"
     $temp_prov_id = "test-dummy-provisioner"
     $temp_worker_id = "dummy-worker-mtabara1"
 }
