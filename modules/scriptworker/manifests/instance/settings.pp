06d336d5fd4603a8da4273d2a334efa235a76a01
Mihai Tabara <mtabara@mozilla.com>
2017-01-04 15:06:16 +0200

    Change provisioner id to dummy one.

--- modules/scriptworker/manifests/instance/settings.pp
+++ modules/scriptworker/manifests/instance/settings.pp
@@ -4,7 +4,7 @@
 # (private)
 class scriptworker::instance::settings {
     $git_key_repo_url = "https://github.com/mozilla-releng/cot-gpg-keys.git"
-    $provisioner_id = "scriptworker-prov-v1"
+    $provisioner_id = "test-dummy-provisioner"
     $temp_prov_id = "test-dummy-provisioner"
     $temp_worker_id = "dummy-worker-mtabara1"
 }
