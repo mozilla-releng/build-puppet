ecdce581702727ec5feef27456e5820f847266f4
dhouse@mozilla.com <dhouse@mozilla.com>
2017-01-17 12:40:52 -0700

    Bug 1330859: pin new jdk17 packages; r=dividehex

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -127,7 +127,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 53
+            $repoflag = 54
             file {
                 "/etc/.repo-flag":
                     content =>
