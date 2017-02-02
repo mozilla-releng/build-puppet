a55ed58b6169812dc27c8a8fc74f5b4716ed1e1a
Amy Rich <arich@mozilla.com>
2017-02-02 16:55:34 -0500

    bug 975004: remove pkewisch from dev-master2; r=bustage

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -284,7 +284,6 @@ node "dev-master2.bb.releng.use1.mozilla.com" {
 
     # Bug 975004 - Grant pkewisch access to dev-master1
     # Bug 1265758 - Add acccess to the following accounts to dev-master2
-    realize(Users::Person["pkewisch"])
     realize(Users::Person["sledru"])
     realize(Users::Person["gchang"])
     realize(Users::Person["ihsiao"])
