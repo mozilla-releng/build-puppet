e35aaa4151fc91f64ff4a57e2d1ef1f05841d6fb
Mihai Tabara <mtabara@mozilla.com>
2017-02-23 16:45:18 +0200

    Bug 1338186 - make beetmoverworker deal with releases. r=rail
    MozReview-Commit-ID: 3lohYkxfuxi

--- modules/beetmover_scriptworker/manifests/init.pp
+++ modules/beetmover_scriptworker/manifests/init.pp
@@ -25,7 +25,7 @@ class beetmover_scriptworker {
                   "appdirs==1.4.0",
                   "arrow==0.10.0",
                   "async-timeout==1.1.0",
-                  "beetmoverscript==0.2.2",
+                  "beetmoverscript==0.3.0b1",
                   "boto3==1.4.4",
                   "botocore==1.5.7",
                   "chardet==2.3.0",
