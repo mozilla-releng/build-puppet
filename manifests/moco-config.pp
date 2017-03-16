127a2c54c4b691dabe07f7a41063aaa9f8a8a3c7
Chris AtLee <catlee@mozilla.com>
2017-03-16 14:00:53 -0400

    Bug 1347912: Update production with new BBB r=rail
    MozReview-Commit-ID: 4RYKZeFzBQQ

--- manifests/moco-config.pp
+++ manifests/moco-config.pp
@@ -397,7 +397,7 @@ class config inherits config::base {
             ],
         },
         "prod" => {
-            version => "1.5.29",
+            version => "1.6.0",
             client_id => secret("buildbot_bridge_prod_taskcluster_client_id"),
             access_token => secret("buildbot_bridge_prod_taskcluster_access_token"),
             dburi => secret("buildbot_bridge_prod_dburi"),
