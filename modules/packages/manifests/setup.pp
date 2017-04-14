8ab56499d7cd227ac7e788d75950dbbbd553d045
Dustin J. Mitchell <dustin@mozilla.com>
2017-04-14 18:06:35 +0000

    Bug 1355838: bump taskcluster-host-secrets to 1.1.0; a=versionbump
    MozReview-Commit-ID: 9qfs6gX43j9
    
    --HG--
    branch : production
    extra : transplant_source : %EA%2A%5B%ED%E2%05%29%29%02%FC%23_%B4%C1%1E%B83%CF%B8%DC

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -166,7 +166,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 83
+            $repoflag = 84
             file {
                 "/etc/.repo-flag":
                     content =>
