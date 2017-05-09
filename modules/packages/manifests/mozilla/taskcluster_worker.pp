523f917090b55c03dba3e4db414dacef39f06aaa
Dustin J. Mitchell <dustin@mozilla.com>
2017-05-09 14:33:50 +0000

    Bug 1363389: bump tc-worker to 0.1.1; r=dragrom
    MozReview-Commit-ID: 1KyinCWaA06

--- modules/packages/manifests/mozilla/taskcluster_worker.pp
+++ modules/packages/manifests/mozilla/taskcluster_worker.pp
@@ -10,7 +10,7 @@ class packages::mozilla::taskcluster_worker {
         'packages::mozilla::taskcluster_worker::end': ;
     }
 
-    $version = '0.1.0'
+    $version = '0.1.1'
 
     # Binaries should be downloaded from
     # https://github.com/taskcluster/taskcluster-worker/releases/download/${version}/taskcluster-worker-${plat}
