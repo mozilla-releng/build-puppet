e92e6b380e8944f12120d7bd666a8cc6de352809
Aki Sasaki <asasaki@mozilla.com>
2017-05-16 10:14:22 -0700

    bug 1365290 - bump mozapkpublisher and taskcluster versions. r=jlorenzo
    --HG--
    extra : rebase_source : 147da06778ff301545b541e336b9c8f730c36152

--- modules/balrog_scriptworker/manifests/init.pp
+++ modules/balrog_scriptworker/manifests/init.pp
@@ -41,7 +41,7 @@ class balrog_scriptworker {
                   "scriptworker==4.0.0",
                   "six==1.10.0",
                   "slugid==1.0.7",
-                  "taskcluster==1.3.1",
+                  "taskcluster==1.3.2",
                   "virtualenv==15.1.0",
                   "yarl==0.10.2",
             ];
