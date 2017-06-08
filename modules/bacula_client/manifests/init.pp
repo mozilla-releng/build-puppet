ae820284cb1df54a94f9e52d6f28d0a3c575fa3c
Dragos Crisan <dcrisan@mozilla.com>
2017-06-08 17:22:11 +0300

    Bug 1368935 - Eliminated lint parser warnings from bacula_client module, r=jwatkins

--- modules/bacula_client/manifests/init.pp
+++ modules/bacula_client/manifests/init.pp
@@ -8,68 +8,68 @@ class bacula_client($cert, $key) {
     include packages::bacula_enterprise_client
     include bacula_client::settings
 
-    case $operatingsystem {
+    case $::operatingsystem {
         CentOS, Darwin: {
 
             $bacula_director = $::config::bacula_director
             $bacula_fd_port = $::config::bacula_fd_port
 
-            $bacula_director_password = secret("bacula_director_password")
+            $bacula_director_password = secret('bacula_director_password')
 
             file {
                 "${bacula_client::settings::confpath}/bacula-fd.conf":
-                    ensure     => file,
-                    owner      => $bacula_client::settings::owner,
-                    group      => $bacula_client::settings::group,
-                    mode       => '0640',
-                    notify     => Service[$bacula_client::settings::servicename],
-                    show_diff  => false,
-                    require    => Class['packages::bacula_enterprise_client'],
-                    content    => template('bacula_client/bacula-fd.conf.erb');
+                    ensure    => file,
+                    owner     => $bacula_client::settings::owner,
+                    group     => $bacula_client::settings::group,
+                    mode      => '0640',
+                    notify    => Service[$bacula_client::settings::servicename],
+                    show_diff => false,
+                    require   => Class['packages::bacula_enterprise_client'],
+                    content   => template('bacula_client/bacula-fd.conf.erb');
                 '/opt/bacula/ssl':
-                    ensure     => directory,
-                    owner      => $bacula_client::settings::owner,
-                    group      => $bacula_client::settings::group,
-                    mode       => '0640',
-                    purge      => true,
-                    recurse    => true,
-                    require    => Class['packages::bacula_enterprise_client'];
+                    ensure  => directory,
+                    owner   => $bacula_client::settings::owner,
+                    group   => $bacula_client::settings::group,
+                    mode    => '0640',
+                    purge   => true,
+                    recurse => true,
+                    require => Class['packages::bacula_enterprise_client'];
                 '/opt/bacula/ssl/cacert.pem':
-                    ensure     => file,
-                    owner      => $bacula_client::settings::owner,
-                    group      => $bacula_client::settings::group,
-                    mode       => '0640',
-                    notify     => Service[$bacula_client::settings::servicename],
-                    require    => File['/opt/bacula/ssl'],
-                    content    => $::config::bacula_cacert;
-                "/opt/bacula/ssl/${fqdn}-crt.pem":
-                    ensure     => file,
-                    owner      => $bacula_client::settings::owner,
-                    group      => $bacula_client::settings::group,
-                    mode       => '0640',
-                    notify     => Service[$bacula_client::settings::servicename],
-                    require    => File['/opt/bacula/ssl'],
-                    content    => $cert;
-                "/opt/bacula/ssl/${fqdn}-key.pem":
-                    ensure     => file,
-                    owner      => $bacula_client::settings::owner,
-                    group      => $bacula_client::settings::group,
-                    mode       => '0600',
-                    notify     => Service[$bacula_client::settings::servicename],
-                    show_diff  => false,
-                    require    => File['/opt/bacula/ssl'],
-                    content    => $key;
+                    ensure  => file,
+                    owner   => $bacula_client::settings::owner,
+                    group   => $bacula_client::settings::group,
+                    mode    => '0640',
+                    notify  => Service[$bacula_client::settings::servicename],
+                    require => File['/opt/bacula/ssl'],
+                    content => $::config::bacula_cacert;
+                "/opt/bacula/ssl/${::fqdn}-crt.pem":
+                    ensure  => file,
+                    owner   => $bacula_client::settings::owner,
+                    group   => $bacula_client::settings::group,
+                    mode    => '0640',
+                    notify  => Service[$bacula_client::settings::servicename],
+                    require => File['/opt/bacula/ssl'],
+                    content => $cert;
+                "/opt/bacula/ssl/${::fqdn}-key.pem":
+                    ensure    => file,
+                    owner     => $bacula_client::settings::owner,
+                    group     => $bacula_client::settings::group,
+                    mode      => '0600',
+                    notify    => Service[$bacula_client::settings::servicename],
+                    show_diff => false,
+                    require   => File['/opt/bacula/ssl'],
+                    content   => $key;
             }
 
             service {
                 $bacula_client::settings::servicename:
-                    ensure => running,
-                    enable => true,
+                    ensure  => running,
+                    enable  => true,
                     require => Class['packages::bacula_enterprise_client'];
             }
         }
         default: {
-            fail("Bacula not supported on $operatingsystem")
+            fail("Bacula not supported on ${::operatingsystem}")
         }
     }
 }
