33240d4a7095c2632fedfdf319b9629cb02cabd1
Dragos Crisan <dcrisan@mozilla.com>
2017-06-08 17:38:54 +0300

    Bug 1368935 - Eliminated lint parser warnings from beetmover_scriptworker module, r=jwatkins

--- modules/beetmover_scriptworker/manifests/init.pp
+++ modules/beetmover_scriptworker/manifests/init.pp
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class beetmover_scriptworker {
     include beetmover_scriptworker::settings
     include dirs::builds
@@ -11,51 +15,51 @@ class beetmover_scriptworker {
     $env_config = $beetmover_scriptworker::settings::env_config[$beetmoverworker_env]
 
     python35::virtualenv {
-        "${beetmover_scriptworker::settings::root}":
-            python3  => "${packages::mozilla::python35::python3}",
-            require  => Class["packages::mozilla::python35"],
-            user     => "${users::builder::username}",
-            group    => "${users::builder::group}",
-            mode     => 700,
+        $beetmover_scriptworker::settings::root:
+            python3  => $packages::mozilla::python35::python3,
+            require  => Class['packages::mozilla::python35'],
+            user     => $users::builder::username,
+            group    => $users::builder::group,
+            mode     => '0700',
             packages => [
-                  "Jinja2==2.9.6",
-                  "PyYAML==3.12",
-                  "MarkupSafe==1.0",
-                  "appdirs==1.4.3",
-                  "aiohttp==2.0.7",
-                  "arrow==0.10.0",
-                  "async-timeout==1.2.1",
-                  "beetmoverscript==0.5.5",
-                  "boto3==1.4.4",
-                  "botocore==1.5.48",
-                  "chardet==2.3.0",
-                  "defusedxml==0.5.0",
-                  "docutils==0.13.1",
-                  "frozendict==1.2",
-                  "future==0.16.0",
-                  "jmespath==0.9.2",
-                  "jsonschema==2.6.0",
-                  "mohawk==0.3.4",
-                  "multidict==2.1.5",
-                  "packaging==16.8",
-                  "pexpect==4.2.1",
-                  "ptyprocess==0.5.1",
-                  "pyparsing==2.2.0",
-                  "python-dateutil==2.6.0",
-                  "python-gnupg==0.4.0",
-                  "requests==2.14.2",
-                  "s3transfer==0.1.10",
-                  "scriptworker==4.1.1",
-                  "six==1.10.0",
-                  "slugid==1.0.7",
-                  "taskcluster==1.3.2",
-                  "virtualenv==15.1.0",
-                  "yarl==0.10.2",
+                  'Jinja2==2.9.6',
+                  'PyYAML==3.12',
+                  'MarkupSafe==1.0',
+                  'appdirs==1.4.3',
+                  'aiohttp==2.0.7',
+                  'arrow==0.10.0',
+                  'async-timeout==1.2.1',
+                  'beetmoverscript==0.5.5',
+                  'boto3==1.4.4',
+                  'botocore==1.5.48',
+                  'chardet==2.3.0',
+                  'defusedxml==0.5.0',
+                  'docutils==0.13.1',
+                  'frozendict==1.2',
+                  'future==0.16.0',
+                  'jmespath==0.9.2',
+                  'jsonschema==2.6.0',
+                  'mohawk==0.3.4',
+                  'multidict==2.1.5',
+                  'packaging==16.8',
+                  'pexpect==4.2.1',
+                  'ptyprocess==0.5.1',
+                  'pyparsing==2.2.0',
+                  'python-dateutil==2.6.0',
+                  'python-gnupg==0.4.0',
+                  'requests==2.14.2',
+                  's3transfer==0.1.10',
+                  'scriptworker==4.1.1',
+                  'six==1.10.0',
+                  'slugid==1.0.7',
+                  'taskcluster==1.3.2',
+                  'virtualenv==15.1.0',
+                  'yarl==0.10.2',
             ];
     }
 
     scriptworker::instance {
-        "${beetmover_scriptworker::settings::root}":
+        $beetmover_scriptworker::settings::root:
             instance_name            => $module_name,
             basedir                  => $beetmover_scriptworker::settings::root,
 
@@ -72,18 +76,18 @@ class beetmover_scriptworker {
 
             task_max_timeout         => $beetmover_scriptworker::settings::task_max_timeout,
 
-            cot_job_type             => "beetmover",
+            cot_job_type             => 'beetmover',
 
             verbose_logging          => $beetmover_scriptworker::settings::verbose_logging,
     }
 
     file {
         "${beetmover_scriptworker::settings::root}/script_config.json":
-            require     => Python35::Virtualenv["${beetmover_scriptworker::settings::root}"],
-            mode        => 600,
-            owner       => "${users::builder::username}",
-            group       => "${users::builder::group}",
-            content     => template("${module_name}/script_config.json.erb"),
-            show_diff   => true;
+            require   => Python35::Virtualenv[$beetmover_scriptworker::settings::root],
+            mode      => '0600',
+            owner     => $users::builder::username,
+            group     => $users::builder::group,
+            content   => template("${module_name}/script_config.json.erb"),
+            show_diff => true;
     }
 }
