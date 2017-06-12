588db1b752dd0ee306e5e3e1cbd0df9251c7416a
Dragos Crisan <dcrisan@mozilla.com>
2017-06-12 09:56:55 +0300

    Bug 1368935 - Eliminated lint parser warnings from windowsutils module, r=jwatkins

--- modules/windowsutils/manifests/startup_tasks.pp
+++ modules/windowsutils/manifests/startup_tasks.pp
@@ -2,21 +2,21 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-define windowsutils::startup_tasks ($ensure, $command, $runas="SYSTEM") {
+define windowsutils::startup_tasks ($ensure, $command, $runas='SYSTEM') {
     if $ensure == present {
         exec { "create_${title}":
-            path    => "C:/Windows/System32/",
-            command => "schtasks.exe /CREATE /RU $runas /SC ONSTART /TN \"${title}\" /TR \"${command}\"",
-            unless  => "schtasks.exe /QUERY /TN ${title}",
+            path      => 'C:/Windows/System32/',
+            command   => "schtasks.exe /CREATE /RU ${runas} /SC ONSTART /TN \"${title}\" /TR \"${command}\"",
+            unless    => "schtasks.exe /QUERY /TN ${title}",
             logoutput => true,
         }
     }
 
     if $ensure == absent {
         exec { "delete_${title}":
-            path    => "C:/Windows/System32/",
-            command => "schtasks.exe /DELETE /F /TN \"${title}\"",
-            onlyif  => "schtasks.exe /QUERY /TN \"${title}\"",
+            path      => 'C:/Windows/System32/',
+            command   => "schtasks.exe /DELETE /F /TN \"${title}\"",
+            onlyif    => "schtasks.exe /QUERY /TN \"${title}\"",
             logoutput => true,
         }
     }
