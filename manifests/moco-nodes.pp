4a1ac2857509b2ee01925ca9006cd9acfd99c928
Kim Moir <kmoir@mozilla.com>
2017-06-14 15:31:59 -0400

    Bug 1364924 - re-image mac test machines to support move to tc workers on mac hardware-batch#15 ,r=aselagea

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -4,8 +4,8 @@
 
 ## TaskCluster workers
 
-#OS X batch#12 10 machines(adding yosemite-r7-(0140-0149)) migrate in taskcluster- Bug 1364924
-node /t-yosemite-r7-0(0[^0-2]\d|1[0-7]\d)\.test\.releng\.scl3\.mozilla\.com/ {
+#OS X batch#15 10 machines(adding yosemite-r7-(0180-0189)) migrate in taskcluster- Bug 1364924
+node /t-yosemite-r7-0(0[^0-2]\d|1[0-8]\d)\.test\.releng\.scl3\.mozilla\.com/ {
     $aspects = [ 'low-security' ]
     $slave_trustlevel = 'try'
     include toplevel::worker::releng::generic_worker::test::gpu
