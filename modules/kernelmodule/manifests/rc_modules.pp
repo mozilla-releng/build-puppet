8b8d31c2b5db02bb054f95dbe3c40a24c34c3eb1
Dragos Crisan <dcrisan@mozilla.com>
2017-06-15 14:32:32 +0300

    Bug 1368935 - Eliminated lint parser warnings from jacuzzi_metadata module, r=jwatkins

--- modules/kernelmodule/manifests/rc_modules.pp
+++ modules/kernelmodule/manifests/rc_modules.pp
@@ -1,7 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class kernelmodule::rc_modules {
     file {
-        "/etc/rc.modules":
+        '/etc/rc.modules':
             ensure => present,
-            mode => 0755;
+            mode   => '0755';
     }
 }
