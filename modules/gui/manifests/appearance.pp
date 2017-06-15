13328889631e1c517c8b5dc889b2fe3730074436
Dragos Crisan <dcrisan@mozilla.com>
2017-06-15 14:45:02 +0300

    Bug 1368935 - Eliminated lint parser warnings from gui module, r=jwatkins

--- modules/gui/manifests/appearance.pp
+++ modules/gui/manifests/appearance.pp
@@ -7,29 +7,29 @@ class gui::appearance {
 
     case $::operatingsystem {
         Darwin: {
-            if ($::macosx_productversion_major == "10.10") {
-                if (!$puppetizing) {
+            if ($::macosx_productversion_major == '10.10') {
+                if (!$::puppetizing) {
                     exec {
-                        "set-background-image" :
+                        'set-background-image' :
                             command => '/bin/ln -sf "/Library/Desktop Pictures/Solid Colors/Solid Aqua Blue.png" /System/Library/CoreServices/DefaultDesktop.jpg',
-                            unless => '/bin/ls -la /System/Library/CoreServices/DefaultDesktop.jpg | /usr/bin/egrep "/Library/Desktop Pictures/Solid Colors/Solid Aqua Blue.png"',
-                            notify => Exec["restart-Dock"];
-                        "restart-Dock" :
-                            command => "/usr/bin/killall Dock",
+                            unless  => '/bin/ls -la /System/Library/CoreServices/DefaultDesktop.jpg | /usr/bin/egrep "/Library/Desktop Pictures/Solid Colors/Solid Aqua Blue.png"',
+                            notify  => Exec['restart-Dock'];
+                        'restart-Dock' :
+                            command     => '/usr/bin/killall Dock',
                             refreshonly => true;
                     }
                 }
                 osxutils::defaults {
-                    "${username}-enable-showscrollbars":
-                        domain => "$::users::builder::home/Library/Preferences/.GlobalPreferences.plist",
-                        key => "AppleShowScrollBars",
-                        value => "Always",
+                    "${::username}-enable-showscrollbars":
+                        domain  => "${::users::builder::home}/Library/Preferences/.GlobalPreferences.plist",
+                        key     => 'AppleShowScrollBars',
+                        value   => 'Always',
                         require => Class['users::builder'];
                 }
                 file {
-                    "$::users::builder::home/Library/Preferences/.GlobalPreferences.plist":
-                        owner => "$users::builder::username",
-                        group => "$users::builder::group";
+                    "${::users::builder::home}/Library/Preferences/.GlobalPreferences.plist":
+                        owner => $users::builder::username,
+                        group => $users::builder::group;
                 }
             }
         }
@@ -37,19 +37,19 @@ class gui::appearance {
             include packages::libglib20_bin
 
             file {
-                "/usr/share/glib-2.0/schemas/99_gsettings.gschema.override":
+                '/usr/share/glib-2.0/schemas/99_gsettings.gschema.override':
                     notify => Exec['update-gsettings'],
-                    source => "puppet:///modules/gui/gsettings.gschema.override";
+                    source => 'puppet:///modules/gui/gsettings.gschema.override';
             }
             exec {
-                "update-gsettings":
-                    command => "/usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas",
-                    require => Class['packages::libglib20_bin'],
+                'update-gsettings':
+                    command     => '/usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas',
+                    require     => Class['packages::libglib20_bin'],
                     refreshonly => true;
             }
         }
         default: {
-            fail("Don't know how to set up GUI appearance on $::operatingsystem")
+            fail("Don't know how to set up GUI appearance on ${::operatingsystem}")
         }
     }
 }
