1d882ed2c1595c27549f2b833ca99312291b79dd
Dragos Crisan <dcrisan@mozilla.com>
2017-06-15 14:31:59 +0300

    Bug 1368935 - Eliminated lint parser warnings from instance_metadata module, r=jwatkins

--- modules/instance_metadata/manifests/disable.pp
+++ modules/instance_metadata/manifests/disable.pp
@@ -1,14 +1,18 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 class instance_metadata::disable {
     file {
-        "/etc/init.d/instance_metadata":
+        '/etc/init.d/instance_metadata':
             ensure => absent;
-        "/etc/instance_metadata.json":
+        '/etc/instance_metadata.json':
             ensure => absent;
-        "/usr/local/bin/instance_metadata.py":
+        '/usr/local/bin/instance_metadata.py':
             ensure => absent;
     }
     service {
-        "instance_metadata":
+        'instance_metadata':
             ensure => stopped,
             enable => false;
     }
--- modules/instance_metadata/manifests/init.pp
+++ modules/instance_metadata/manifests/init.pp
@@ -3,17 +3,17 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 class instance_metadata {
-    $inst_metadata_py = $operatingsystem ? {
-        windows => "C:/etc/instance_metadata.py",
-        default => "/usr/local/bin/instance_metadata.py",
+    $inst_metadata_py = $::operatingsystem ? {
+        windows => 'C:/etc/instance_metadata.py',
+        default => '/usr/local/bin/instance_metadata.py',
     }
-    $etc_dir = $operatingsystem ? {
-        windows => "C:/etc",
-        default => "/etc",
+    $etc_dir = $::operatingsystem ? {
+        windows => 'C:/etc',
+        default => '/etc',
     }
-    $py_user =  $operatingsystem ? {
+    $py_user =  $::operatingsystem ? {
         windows => undef,
-        default => "root",
+        default => 'root',
     }
     case $::fqdn {
         /.*\.releng\.(use1|usw2)\.mozilla\.com$/: {
@@ -23,19 +23,19 @@ class instance_metadata {
 
             file {
                 $inst_metadata_py:
-                    owner   => "root",
-                    mode    => 0755,
-                    source  => "puppet:///modules/instance_metadata/instance_metadata.py",
+                    owner   => 'root',
+                    mode    => '0755',
+                    source  => 'puppet:///modules/instance_metadata/instance_metadata.py',
                     require => Class[dirs::etc];
             }
 
             # Run this from exec so that we get it at least once when puppet runs the first time.
             exec {
-                "get_instance_metadata":
+                'get_instance_metadata':
                     require => File[$inst_metadata_py],
-                    creates => "$etc_dir/instance_metadata.json",
+                    creates => "${etc_dir}/instance_metadata.json",
                     user    => $py_user,
-                    command => "$python $inst_metadata_py -o $etc_dir/instance_metadata.json";
+                    command => "${python} ${inst_metadata_py} -o ${etc_dir}/instance_metadata.json";
             }
 
             # On Ubuntu systems, run from init.d on boot, but on CentOS, run in
@@ -44,18 +44,18 @@ class instance_metadata {
             case $::operatingsystem {
                 Ubuntu: {
                     file {
-                        "/etc/init.d/instance_metadata":
-                            require => File["/usr/local/bin/instance_metadata.py"],
-                            content => template("instance_metadata/instance_metadata.initd.erb"),
-                            mode    => 0755,
-                            owner   => "root",
-                            notify  => Service["instance_metadata"];
+                        '/etc/init.d/instance_metadata':
+                            require => File['/usr/local/bin/instance_metadata.py'],
+                            content => template('instance_metadata/instance_metadata.initd.erb'),
+                            mode    => '0755',
+                            owner   => 'root',
+                            notify  => Service['instance_metadata'];
                     }
                     service {
-                        "instance_metadata":
-                            require => [
-                                File["/etc/init.d/instance_metadata"],
-                                File["/usr/local/bin/instance_metadata.py"],
+                        'instance_metadata':
+                            require   => [
+                                File['/etc/init.d/instance_metadata'],
+                                File['/usr/local/bin/instance_metadata.py'],
                             ],
                             hasstatus => false,
                             enable    => true;
@@ -63,19 +63,19 @@ class instance_metadata {
                 }
                 CentOS: {
                     runner::task {
-                        "00-instance_metadata":
-                            require => File["/usr/local/bin/instance_metadata.py"],
+                        '00-instance_metadata':
+                            require => File['/usr/local/bin/instance_metadata.py'],
                             content => template("${module_name}/runner_task.sh.erb");
                     }
 
                     # temporary, to uninstall the init task
                     file {
-                        "/etc/init.d/instance_metadata":
+                        '/etc/init.d/instance_metadata':
                             ensure => absent,
-                            notify => Service["instance_metadata"];
+                            notify => Service['instance_metadata'];
                     }
                     service {
-                        "instance_metadata":
+                        'instance_metadata':
                             enable => false;
                     }
                 }
@@ -83,7 +83,7 @@ class instance_metadata {
                     # Future runner task. Currently luanched by startbuildbot.bat
                 }
                 default: {
-                    fail("instance_metadata is not supported on $::operatingsystem")
+                    fail("instance_metadata is not supported on ${::operatingsystem}")
                 }
             }
         }
@@ -93,8 +93,8 @@ class instance_metadata {
             file {
                 "${dirs::etc::dir}/instance_metadata.json":
                     owner   => root,
-                    mode    => 0644,
-                    content => "{}";
+                    mode    => '0644',
+                    content => '{}';
             }
         }
     }
