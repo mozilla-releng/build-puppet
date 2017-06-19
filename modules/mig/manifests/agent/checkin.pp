ed58254a0595bd3486a3b8a231b4c4b8fec2b270
Dragos Crisan <dcrisan@mozilla.com>
2017-06-19 10:24:00 +0300

    Bug 1368935 - Eliminated lint parser warnings from mig module, r=jwatkins

--- modules/mig/manifests/agent/checkin.pp
+++ modules/mig/manifests/agent/checkin.pp
@@ -5,18 +5,18 @@ class mig::agent::checkin {
     # when running in checkin mode, each invocation of mig-agent performs
     # one check-in, runs all pending actions, and exits
     class { 'mig::agent::base':
-        isimmortal => "off",
-        installservice => "off",
-        discoverpublicip => "on",
-        discoverawsmeta => "off",
-        checkin => "on",
-        moduletimeout => "300s",
-        apiurl => "https://api.mig.mozilla.org/api/v1/"
+        isimmortal       => 'off',
+        installservice   => 'off',
+        discoverpublicip => 'on',
+        discoverawsmeta  => 'off',
+        checkin          => 'on',
+        moduletimeout    => '300s',
+        apiurl           => 'https://api.mig.mozilla.org/api/v1/'
     }
     # ensure some of the service files aren't present
     file {
-        [ "/etc/cron.d/mig-agent", "/etc/init/mig-agent.conf",
-          "/etc/init.d/mig-agent", "/etc/systemd/system/mig-agent.service"]:
+        [ '/etc/cron.d/mig-agent', '/etc/init/mig-agent.conf',
+          '/etc/init.d/mig-agent', '/etc/systemd/system/mig-agent.service']:
             ensure => absent
     }
     include runner::tasks::mig_agent
--- modules/mig/manifests/agent/daemon.pp
+++ modules/mig/manifests/agent/daemon.pp
@@ -3,21 +3,21 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class mig::agent::daemon {
     class { 'mig::agent::base':
-        isimmortal => "on",
-        installservice => "on",
-        discoverpublicip => "on",
-        discoverawsmeta => "on",
-        checkin => "off",
-        moduletimeout => "1200s",
-        apiurl => "https://api.mig.mozilla.org/api/v1/"
+        isimmortal       => 'on',
+        installservice   => 'on',
+        discoverpublicip => 'on',
+        discoverawsmeta  => 'on',
+        checkin          => 'off',
+        moduletimeout    => '1200s',
+        apiurl           => 'https://api.mig.mozilla.org/api/v1/'
     }
     # on package update, shutdown the old agent and start the new one
     # when the package is upgraded, exec a new instance of the agent
-    $mig_path = $operatingsystem ? { /(CentOS|Ubuntu)/ => '/sbin/mig-agent', Darwin => '/usr/local/bin/mig-agent' }
+    $mig_path = $::operatingsystem ? { /(CentOS|Ubuntu)/ => '/sbin/mig-agent', Darwin => '/usr/local/bin/mig-agent' }
     exec {
         'restart mig':
-            command => "${mig_path} -q=shutdown; ${mig_path}",
-            subscribe => Class['packages::mozilla::mig_agent'],
+            command     => "${mig_path} -q=shutdown; ${mig_path}",
+            subscribe   => Class['packages::mozilla::mig_agent'],
             refreshonly => true
     }
 }
