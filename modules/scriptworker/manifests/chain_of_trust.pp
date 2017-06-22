3fe94282494914092b1ef884f9a447c67e834429
Dragos Crisan <dcrisan@mozilla.com>
2017-06-22 12:46:20 +0300

    Bug 1368935 - Eliminated lint parser warnings from scriptworker module, r=jwatkins

--- modules/scriptworker/manifests/chain_of_trust.pp
+++ modules/scriptworker/manifests/chain_of_trust.pp
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 define scriptworker::chain_of_trust(
   $basedir,
 
@@ -21,7 +24,7 @@ define scriptworker::chain_of_trust(
 
   File {
     ensure      => present,
-    mode        => 600,
+    mode        => '0600',
     owner       => $username,
     group       => $group,
     show_diff   => false,
@@ -33,39 +36,39 @@ define scriptworker::chain_of_trust(
         ensure      => directory;
     # these are the pubkeys that can sign git commits
     $git_pubkey_dir:
-        ensure      => directory,
-        source      => 'puppet:///modules/scriptworker/git_pubkeys',
-        recurse     => true,
+        ensure       => directory,
+        source       => 'puppet:///modules/scriptworker/git_pubkeys',
+        recurse      => true,
         recurselimit => 1,
-        purge       => true,
-        require     => Python35::Virtualenv[$basedir];
+        purge        => true,
+        require      => Python35::Virtualenv[$basedir];
     # cron jobs to poll git + rebuild gpg homedirs
-    "/etc/cron.d/scriptworker":
-        content     => template("scriptworker/scriptworker.cron.erb");
+    '/etc/cron.d/scriptworker':
+        content     => template('scriptworker/scriptworker.cron.erb');
     # Notify rebuild_gpg_homedirs if the pubkey dir changes
     "${basedir}/.git-pubkey-dir-checksum":
         notify  => Exec['rebuild_gpg_homedirs'];
     "/home/${username}/pubkey":
-        mode        => 644,
-        content     => $pubkey,
-        show_diff   => true;
+        mode      => '0644',
+        content   => $pubkey,
+        show_diff => true;
     "/home/${username}/privkey":
-        content     => $privkey;
+        content => $privkey;
   }
 
   exec {
       # create gpg homedirs on change
       'rebuild_gpg_homedirs':
-          require => [Python35::Virtualenv[$basedir],
+          require   => [Python35::Virtualenv[$basedir],
                       Git::Repo["scriptworker-${git_key_repo_dir}"],
                       File["${basedir}/scriptworker.yaml"]],
-          command => "${basedir}/bin/rebuild_gpg_homedirs ${basedir}/scriptworker.yaml",
+          command   => "${basedir}/bin/rebuild_gpg_homedirs ${basedir}/scriptworker.yaml",
           subscribe => File[$git_pubkey_dir],
-          user    => $username;
+          user      => $username;
       # Create checksum file of git pubkeys
       "${basedir}/.git-pubkey-dir-checksum":
           require => File[$git_pubkey_dir],
-          path    => "/usr/local/bin/:/bin:/usr/sbin:/usr/bin",
+          path    => '/usr/local/bin/:/bin:/usr/sbin:/usr/bin',
           user    => $username,
           command => "find ${git_pubkey_dir} -type f | xargs md5sum | sort > ${basedir}/.git-pubkey-dir-checksum";
   }
