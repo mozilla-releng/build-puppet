db35bb1bc5e8542f3318bc4b4795c68765db5722
Dragos Crisan <dcrisan@mozilla.com>
2017-06-22 12:47:58 +0300

    Bug 1368935 - Eliminated lint parser warnings from shared module, r=jwatkins

--- modules/shared/manifests/init.pp
+++ modules/shared/manifests/init.pp
@@ -4,9 +4,9 @@
 class shared {
     # the appropriate /usr subdir for libraries; this is only useful
     # on linux platforms.
-    $lib_arch_dir = $hardwaremodel ? {
-        i686   => "lib",
-        x86_64 => "lib64",
-        default => "lib"
+    $lib_arch_dir = $::hardwaremodel ? {
+        i686    => 'lib',
+        x86_64  => 'lib64',
+        default => 'lib'
     }
 }
