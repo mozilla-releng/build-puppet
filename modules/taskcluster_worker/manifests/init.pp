82e6a158ae36afb00d610329af192fb7b2a9175b
Dragos Crisan <dcrisan@mozilla.com>
2017-06-22 15:07:46 +0300

    Bug 1368935 - Eliminated lint parser warnings from taskcluster_worker, r=jwatkins

--- modules/taskcluster_worker/manifests/init.pp
+++ modules/taskcluster_worker/manifests/init.pp
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class taskcluster_worker {
     include packages::mozilla::taskcluster_worker
     include ::dirs::usr::local::bin
@@ -7,27 +10,27 @@ class taskcluster_worker {
 
     $puppet_servers = $::config::puppet_servers
     file { '/etc/taskcluster-worker.yml':
-        ensure => present,
+        ensure  => present,
         content => template('taskcluster_worker/taskcluster-worker.yml.erb'),
-        mode => 0644,
-        owner => $::users::root::username,
-        group => $::users::root::group
+        mode    => '0644',
+        owner   => $::users::root::username,
+        group   => $::users::root::group
     }
 
     case $::operatingsystem {
         Darwin: {
             file { '/Library/LaunchAgents/net.taskcluster.worker.plist':
-                ensure => present,
+                ensure  => present,
                 content => template('taskcluster_worker/taskcluster-worker.plist.erb'),
-                mode => 0644,
-                owner => $::users::root::username,
-                group => $::users::root::group
+                mode    => '0644',
+                owner   => $::users::root::username,
+                group   => $::users::root::group
             }
-            service { "net.taskcluster.worker":
-                require   => [
-                    File["/Library/LaunchAgents/net.taskcluster.worker.plist"],
+            service { 'net.taskcluster.worker':
+                require => [
+                    File['/Library/LaunchAgents/net.taskcluster.worker.plist'],
                 ],
-                enable    => true;
+                enable  => true;
             }
         }
 
@@ -38,20 +41,20 @@ class taskcluster_worker {
                     ensure => directory,
                     owner  => $users::builder::username,
                     group  => $users::builder::group;
-                "/usr/local/bin/run-tc-worker.sh":
+                '/usr/local/bin/run-tc-worker.sh':
                     source => 'puppet:///modules/taskcluster_worker/run-tc-worker.sh',
-                    mode    => 0755,
-                    owner   => $users::root::username,
-                    group   => $users::root::group;
+                    mode   => '0755',
+                    owner  => $users::root::username,
+                    group  => $users::root::group;
                 "${::users::builder::home}/.config/autostart/gnome-terminal.desktop":
-                    content => template("taskcluster_worker/gnome-terminal.desktop.erb"),
+                    content => template('taskcluster_worker/gnome-terminal.desktop.erb'),
                     owner   => $users::builder::username,
                     group   => $users::builder::group;
             }
         }
 
         default: {
-            fail("cannot install on $::operatingsystem")
+            fail("cannot install on ${::operatingsystem}")
         }
     }
 }
