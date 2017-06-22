6d0a1a3753eac4d790cc9d6bd3213d758cdd61d3
Dragos Crisan <dcrisan@mozilla.com>
2017-06-22 15:07:24 +0300

    Bug 1368935 - Eliminated lint parser warnings from taskcluster_host_secrets module, r=jwatkins

--- modules/taskcluster_host_secrets/manifests/init.pp
+++ modules/taskcluster_host_secrets/manifests/init.pp
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class taskcluster_host_secrets {
     include packages::nodejs
     include packages::mozilla::taskcluster_host_secrets
@@ -5,20 +8,20 @@ class taskcluster_host_secrets {
     case $::operatingsystem {
         CentOS: {
             $taskcluster_access_token = secret('TC_HOST_SECRETS_ACCESS_TOKEN')
-            if ($taskcluster_access_token == "") {
-                fail("missing TC_HOST_SECRETS_ACCESS_TOKEN")
+            if ($taskcluster_access_token == '') {
+                fail('missing TC_HOST_SECRETS_ACCESS_TOKEN')
             }
             $credentials_expiry = '4 days'
-            $allowed_ips = '::ffff:10.0.0.0/8 10.0.0.0/8'
-            $service_port = 8020
+            $allowed_ips        = '::ffff:10.0.0.0/8 10.0.0.0/8'
+            $service_port       = 8020
 
             file { '/etc/host-secrets.conf':
-                ensure => present,
+                ensure  => present,
                 content => template('taskcluster_host_secrets/host-secrets.conf.erb'),
-                mode => 0644,
-                owner => root,
-                group => wheel,
-                notify => Service['host-secrets'],
+                mode    => '0644',
+                owner   => root,
+                group   => wheel,
+                notify  => Service['host-secrets'],
             }
 
             file { '/usr/bin/host-secrets':
@@ -35,7 +38,7 @@ class taskcluster_host_secrets {
             }
         }
         default: {
-            fail("cannot install on $::operatingsystem")
+            fail("Cannot install on ${::operatingsystem}")
         }
     }
 }
