5987f786a3ff395a47a602719772818bb77f6c34
Dragos Crisan <dcrisan@mozilla.com>
2017-06-22 14:45:57 +0300

    Bug 1368935 - Eliminated lint parser warnings from signing_scriptworker module, r=jwatkins

--- modules/signing_scriptworker/manifests/init.pp
+++ modules/signing_scriptworker/manifests/init.pp
@@ -1,3 +1,6 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class signing_scriptworker {
     include ::config
     include signing_scriptworker::settings
@@ -9,61 +12,61 @@ class signing_scriptworker {
     include packages::make
     include nrpe::check::signing_file_ages
 
-    $env_config = $signing_scriptworker::settings::env_config[$signing_scriptworker_env]
+    $env_config          = $signing_scriptworker::settings::env_config[$signing_scriptworker_env]
 
     # because puppet barfs whenever I try to put it in settings.pp
-    $verbose_logging = true
+    $verbose_logging     = true
     $build_tools_version = '23.0.3'
     # See value defined in packages::mozilla::android_sdk
-    $zipalign_location = "/tools/android-sdk/build-tools/$build_tools_version/zipalign"
-    $dmg_location = "${signing_scriptworker::settings::root}/dmg/dmg"
-    $hfsplus_location = "${signing_scriptworker::settings::root}/dmg/hfsplus"
+    $zipalign_location   = "/tools/android-sdk/build-tools/${build_tools_version}/zipalign"
+    $dmg_location        = "${signing_scriptworker::settings::root}/dmg/dmg"
+    $hfsplus_location    = "${signing_scriptworker::settings::root}/dmg/hfsplus"
 
     python35::virtualenv {
-        "${signing_scriptworker::settings::root}":
-            python3  => "${packages::mozilla::python35::python3}",
-            require  => Class["packages::mozilla::python35"],
-            user     => "${users::signer::username}",
-            group    => "${users::signer::group}",
-            mode     => 700,
+        $signing_scriptworker::settings::root:
+            python3  => $packages::mozilla::python35::python3,
+            require  => Class['packages::mozilla::python35'],
+            user     => $users::signer::username,
+            group    => $users::signer::group,
+            mode     => '0700',
             packages => [
-                  "PyYAML==3.12",
-                  "aiohttp==2.1.0",
-                  "arrow==0.10.0",
-                  "async-timeout==1.2.1",
-                  "certifi==2017.4.17",
-                  "chardet==3.0.4",
-                  "defusedxml==0.5.0",
-                  "ecdsa==0.13",
-                  "frozendict==1.2",
-                  "future==0.16.0",
-                  "jsonschema==2.6.0",
-                  "idna==2.5",
-                  "mohawk==0.3.4",
-                  "multidict==2.1.6",
-                  "pefile==2016.7.26",
-                  "pexpect==4.2.1",
-                  "ptyprocess==0.5.1",
-                  "pycrypto==2.6.1",
-                  "pyparsing==2.2.0",
-                  "python-dateutil==2.6.0",
-                  "python-gnupg==0.4.0",
-                  "python-jose==1.3.2",
-                  "requests==2.18.1",
-                  "scriptworker==4.1.2",
-                  "signingscript==2.0.0",
-                  "signtool==3.1.0",
-                  "six==1.10.0",
-                  "slugid==1.0.7",
-                  "taskcluster==1.3.3",
-                  "urllib3==1.21.1",
-                  "virtualenv==15.1.0",
-                  "yarl==0.10.3",
+                  'PyYAML==3.12',
+                  'aiohttp==2.1.0',
+                  'arrow==0.10.0',
+                  'async-timeout==1.2.1',
+                  'certifi==2017.4.17',
+                  'chardet==3.0.4',
+                  'defusedxml==0.5.0',
+                  'ecdsa==0.13',
+                  'frozendict==1.2',
+                  'future==0.16.0',
+                  'jsonschema==2.6.0',
+                  'idna==2.5',
+                  'mohawk==0.3.4',
+                  'multidict==2.1.6',
+                  'pefile==2016.7.26',
+                  'pexpect==4.2.1',
+                  'ptyprocess==0.5.1',
+                  'pycrypto==2.6.1',
+                  'pyparsing==2.2.0',
+                  'python-dateutil==2.6.0',
+                  'python-gnupg==0.4.0',
+                  'python-jose==1.3.2',
+                  'requests==2.18.1',
+                  'scriptworker==4.1.2',
+                  'signingscript==2.0.0',
+                  'signtool==3.1.0',
+                  'six==1.10.0',
+                  'slugid==1.0.7',
+                  'taskcluster==1.3.3',
+                  'urllib3==1.21.1',
+                  'virtualenv==15.1.0',
+                  'yarl==0.10.3',
             ];
     }
 
     scriptworker::instance {
-        "${signing_scriptworker::settings::root}":
+        $signing_scriptworker::settings::root:
             instance_name            => $module_name,
             basedir                  => $signing_scriptworker::settings::root,
 
@@ -79,7 +82,7 @@ class signing_scriptworker {
             worker_type              => $env_config['worker_type'],
             task_max_timeout         => $signing_scriptworker::settings::task_max_timeout,
 
-            cot_job_type             => "signing",
+            cot_job_type             => 'signing',
 
             sign_chain_of_trust      => $env_config["sign_chain_of_trust"],
             verify_chain_of_trust    => $env_config["verify_chain_of_trust"],
@@ -89,42 +92,42 @@ class signing_scriptworker {
     }
 
     nrpe::custom {
-        "signingworker.cfg":
+        'signingworker.cfg':
             content => template("${module_name}/nagios.cfg.erb");
     }
 
     file {
         "${signing_scriptworker::settings::root}/script_config.json":
-            require     => Python35::Virtualenv["${signing_scriptworker::settings::root}"],
-            mode        => 600,
-            owner       => "${users::signer::username}",
-            group       => "${users::signer::group}",
-            content     => template("${module_name}/script_config.json.erb"),
-            show_diff   => true;
+            require   => Python35::Virtualenv[$signing_scriptworker::settings::root],
+            mode      => '0600',
+            owner     => $users::signer::username,
+            group     => $users::signer::group,
+            content   => template("${module_name}/script_config.json.erb"),
+            show_diff => true;
         "${signing_scriptworker::settings::root}/passwords.json":
-            require     => Python35::Virtualenv["${signing_scriptworker::settings::root}"],
-            mode        => 600,
-            owner       => "${users::signer::username}",
-            group       => "${users::signer::group}",
-            content     => template("${module_name}/${env_config['passwords_template']}"),
+            require   => Python35::Virtualenv[$signing_scriptworker::settings::root],
+            mode      => '0600',
+            owner     => $users::signer::username,
+            group     => $users::signer::group,
+            content   => template("${module_name}/${env_config['passwords_template']}"),
             show_diff => false;
         "${signing_scriptworker::settings::root}/dmg":
-            ensure      => directory,
-            mode        => 755,
-            owner       => "${users::signer::username}",
-            group       => "${users::signer::group}";
-        "${dmg_location}":
-            mode        => 755,
-            owner       => "${users::signer::username}",
-            group       => "${users::signer::group}",
-            source      => "puppet:///modules/signing_scriptworker/dmg/dmg",
-            show_diff   => false;
-        "${hfsplus_location}":
-            mode        => 755,
-            owner       => "${users::signer::username}",
-            group       => "${users::signer::group}",
-            source      => "puppet:///modules/signing_scriptworker/dmg/hfsplus",
-            show_diff   => false;
+            ensure => directory,
+            mode   => '0755',
+            owner  => $users::signer::username,
+            group  => $users::signer::group;
+        $dmg_location:
+            mode      => '0755',
+            owner     => $users::signer::username,
+            group     => $users::signer::group,
+            source    => 'puppet:///modules/signing_scriptworker/dmg/dmg',
+            show_diff => false;
+        $hfsplus_location:
+            mode      => '0755',
+            owner     => $users::signer::username,
+            group     => $users::signer::group,
+            source    => 'puppet:///modules/signing_scriptworker/dmg/hfsplus',
+            show_diff => false;
     }
 
     packages::mozilla::android_sdk {
--- modules/signing_scriptworker/manifests/settings.pp
+++ modules/signing_scriptworker/manifests/settings.pp
@@ -1,40 +1,43 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class signing_scriptworker::settings {
     include ::config
 
-    $root = $config::scriptworker_root
-    $task_max_timeout = 1800
-    $task_script = "${root}/bin/signingscript"
+    $root               = $config::scriptworker_root
+    $task_max_timeout   = 1800
+    $task_script        = "${root}/bin/signingscript"
     $task_script_config = "${root}/script_config.json"
-    $verbose = true
-    $worker_group = "signing-linux-v1"
+    $verbose            = true
+    $worker_group       = 'signing-linux-v1'
 
     $env_config = {
-        "dev" => {
-            worker_type => "signing-linux-dev",
-            taskcluster_client_id => secret("dev_signing_scriptworker_taskcluster_client_id"),
-            taskcluster_access_token => secret("dev_signing_scriptworker_taskcluster_access_token"),
-            passwords_template => "dep-passwords.json.erb",
-            sign_chain_of_trust => false,
-            verify_chain_of_trust => false,
-            verify_cot_signature => false
+        'dev' => {
+            worker_type              => 'signing-linux-dev',
+            taskcluster_client_id    => secret('dev_signing_scriptworker_taskcluster_client_id'),
+            taskcluster_access_token => secret('dev_signing_scriptworker_taskcluster_access_token'),
+            passwords_template       => 'dep-passwords.json.erb',
+            sign_chain_of_trust      => false,
+            verify_chain_of_trust    => false,
+            verify_cot_signature     => false
         },
-        "dep" => {
-            worker_type => "depsigning",
-            taskcluster_client_id => secret("dep_signing_scriptworker_taskcluster_client_id"),
-            taskcluster_access_token => secret("dep_signing_scriptworker_taskcluster_access_token"),
-            passwords_template => "dep-passwords.json.erb",
-            sign_chain_of_trust => false,
-            verify_chain_of_trust => true,
-            verify_cot_signature => false
+        'dep' => {
+            worker_type              => 'depsigning',
+            taskcluster_client_id    => secret('dep_signing_scriptworker_taskcluster_client_id'),
+            taskcluster_access_token => secret('dep_signing_scriptworker_taskcluster_access_token'),
+            passwords_template       => 'dep-passwords.json.erb',
+            sign_chain_of_trust      => false,
+            verify_chain_of_trust    => true,
+            verify_cot_signature     => false
         },
-        "prod" => {
-            worker_type => "signing-linux-v1",
-            taskcluster_client_id => secret("signing_scriptworker_taskcluster_client_id"),
-            taskcluster_access_token => secret("signing_scriptworker_taskcluster_access_token"),
-            passwords_template => "passwords.json.erb",
-            sign_chain_of_trust => true,
-            verify_chain_of_trust => true,
-            verify_cot_signature => true
+        'prod' => {
+            worker_type              => 'signing-linux-v1',
+            taskcluster_client_id    => secret('signing_scriptworker_taskcluster_client_id'),
+            taskcluster_access_token => secret('signing_scriptworker_taskcluster_access_token'),
+            passwords_template       => 'passwords.json.erb',
+            sign_chain_of_trust      => true,
+            verify_chain_of_trust    => true,
+            verify_cot_signature     => true
         }
     }
 }
