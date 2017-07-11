afd7e46f0f330cc730484b44231cd1d17cf15a77
Nick Thomas <nthomas@mozilla.com>
2017-07-11 21:52:35 +1200

    Bug 1377367 - typo fixup, r=me

--- modules/pushapk_scriptworker/manifests/init.pp
+++ modules/pushapk_scriptworker/manifests/init.pp
@@ -96,7 +96,7 @@ class pushapk_scriptworker {
     file {
         $pushapk_scriptworker::settings::script_config:
             require   => Python35::Virtualenv[$pushapk_scriptworker::settings::root],
-            content   => template("${module_name}/script_config.json.erb"),
+            content   => template("${module_name}/script_config.json.erb");
 
         $google_play_config['aurora']['certificate_target_location']:
             content     => $google_play_config['aurora']['certificate'];
