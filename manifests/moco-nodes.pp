1bd3400da8b5dd5391945353ce337ed1961b9215
Andrei Obreja <aobreja@mozilla.com>
2017-07-28 15:29:18 +0300

    Bug 1384640 - migrate more buildbot macosx test machines to taskcluster, r=aselagea

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -4,8 +4,8 @@
 
 ## TaskCluster workers
 
-#OS X batch#27 30 machines(adding yosemite-r7-(0310-0360)) migrate in taskcluster- Bug 1364924
-node /t-yosemite-r7-0(0[^0-2]\d|[1-2]\d+|3[0-7]\d)\.test\.releng\.scl3\.mozilla\.com/ {
+#OS X batch#28 13 machines(adding yosemite-r7-(0380-0392)) migrate in taskcluster- Bug 1384640
+node /t-yosemite-r7-0(0[^0-2]\d|[1-2]\d+|3[0-9]\d)\.test\.releng\.scl3\.mozilla\.com/ {
     $aspects          = [ 'low-security' ]
     $slave_trustlevel = 'try'
     include toplevel::worker::releng::generic_worker::test::gpu
