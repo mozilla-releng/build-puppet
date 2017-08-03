18ca7f699050b396e1db8e50f96c66c76f38a211
Jake Watkins <jwatkins@mozilla.com>
2017-08-03 10:02:36 -0700

    Bug 1369566: osx 10.7 pf support; r=dhouse

--- modules/pf/files/pflog.sh
+++ modules/pf/files/pflog.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 ifconfig pflog0 create
-/usr/sbin/tcpdump -lnettti pflog0 | /usr/bin/logger -t pflog -p local2.info
+/usr/sbin/tcpdump -lnettti pflog0 2> /dev/null | /usr/bin/logger -t pflog -p local2.info
