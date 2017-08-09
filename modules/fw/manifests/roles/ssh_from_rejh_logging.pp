f1c1df6a180590589dc281205ece106b0fa3da91
Jake Watkins <jwatkins@mozilla.com>
2017-08-09 10:25:29 -0700

    Bug 1387251: add rejh limiting rolls; r=dhouse

---
+++ modules/fw/manifests/roles/ssh_from_rejh_logging.pp
@@ -0,0 +1,13 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class fw::roles::ssh_from_rejh_logging {
+    include fw::networks
+
+    fw::rules { 'allow_ssh_from_rejh_logging':
+        sources =>  $::fw::networks::rejh,
+        app     => 'ssh',
+        log     => true
+    }
+}
