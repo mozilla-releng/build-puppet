ef1a2398f146323196158ac19d31c35a7c34cffc
Mihai Tabara <mtabara@mozilla.com>
2017-09-12 17:05:01 +0300

    Fix releaserunner_env for releaserunner2 in services.pp as well

--- modules/releaserunner2/manifests/services.pp
+++ modules/releaserunner2/manifests/services.pp
@@ -8,9 +8,9 @@ class releaserunner2::services {
 
     supervisord::supervise {
         'releaserunner2':
-            command      => "${releaserunner2::settings::tools_dst}/buildfarm/release/release-runner2.sh ${releaserunner2::settings::root} ${releaserunner2::settings::logfile} ${releaserunner2::settings::root}/${config::releaserunner2_env_config[$releaserunner_env]['releaserunner_config_file']}",
+            command      => "${releaserunner2::settings::tools_dst}/buildfarm/release/release-runner2.sh ${releaserunner2::settings::root} ${releaserunner2::settings::logfile} ${releaserunner2::settings::root}/${config::releaserunner2_env_config[$releaserunner2_env]['releaserunner_config_file']}",
             user         => $::config::builder_username,
-            require      => [ File["${releaserunner2::settings::root}/${config::releaserunner2_env_config[$releaserunner_env]['releaserunner_config_file']}"],
+            require      => [ File["${releaserunner2::settings::root}/${config::releaserunner2_env_config[$releaserunner2_env]['releaserunner_config_file']}"],
                               Python::Virtualenv[$releaserunner2::settings::root],
                               Mercurial::Repo['releaserunner-tools']],
             extra_config => template("${module_name}/extra_config.erb")
