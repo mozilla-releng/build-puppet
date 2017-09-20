b4a1062a0575536709b2e0caf8e0db9215d8deaf
Jake Watkins <jwatkins@mozilla.com>
2017-09-20 15:03:38 -0700

    Bug 1392820: allow ssh from slaveapi fw role; r=dhouse

--- modules/fw/manifests/profiles/buildbot_slave.pp
+++ modules/fw/manifests/profiles/buildbot_slave.pp
@@ -7,6 +7,7 @@ class fw::profiles::buildbot_slave {
     case $::fqdn {
         /.*\.scl3\.mozilla\.com/: {
             include ::fw::roles::ssh_from_anywhere_logging
+            include ::fw::roles::ssh_from_slaveapi
             include ::fw::roles::nrpe_from_nagios
             if $::operatingsystem == 'Darwin' {
                 include ::fw::roles::vnc_from_anywhere_logging
