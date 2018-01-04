15f81add39349b245884025ce3a2c3c59f75ddaf
Jake Watkins <jwatkins@mozilla.com>
2018-01-04 12:08:10 -0800

    Bug 1424321: ubuntu preseed cfg and script; r=dhouse
    --HG--
    extra : rebase_source : dba000b3d66ac8b9dfe6bb852c0e7d60e773e561

---
+++ setup/preseed/preseed_to_puppet.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+set -x
+
+fail() {
+    echo "INSTALL FAILED"
+    # if we exit, the install will continue.  That's not what we want.  So busyloop.
+    while true; do
+        sleep 60;
+    done
+}
+
+# get the PUPPET_PASS; this will be our key to getting a certificate down
+# the line..
+for word in $(</proc/cmdline); do
+        case $word in
+                PUPPET_PASS=*)
+                    PUPPET_PASS="${word//PUPPET_PASS=}" ;;
+        esac
+done
+
+# Check that PUPPET_PASS is set
+if [ -z "$PUPPET_PASS" ]; then
+    echo "PUPPET_PASS was not set; aborting setup."
+    fail
+fi
+
+# Check that puppet is installed properly
+if ! puppet --version >/dev/null; then
+    echo "Puppet does not appear to be installed properly."
+    fail
+fi
+
+# Stop and disable puppet daemon under systemd
+systemctl stop puppet || fail
+systemctl disable puppet || fail
+
+# fix up /etc/issue
+(
+    grep -v '^Kickstart' /etc/issue
+    echo "Kickstart Date:" `date`
+    echo "Kickstart OS:" `facter operatingsystem` `facter operatingsystemrelease`
+) > /etc/issue~
+mv /etc/issue~ /etc/issue
+
+# install the deploy key
+echo "$PUPPET_PASS" > /root/deploypass
+chmod 600 /root/deploypass
+
+# set up the puppetize script to run at boot
+hgrepo="http://hg.mozilla.org/build/puppet"
+# try to use the proxy, falling back to a direct fetch (e.g., if the DC doesn't have a proxy)
+http_proxy=http://proxy.dmz.mdc1.mozilla.com:3128/ wget -O/root/puppetize.sh "$hgrepo/raw-file/default/modules/puppet/files/puppetize.sh" \
+    || wget -O/root/puppetize.sh "$hgrepo/raw-file/default/modules/puppet/files/puppetize.sh" \
+    || fail
+chmod +x /root/puppetize.sh
+
+rc_local="/etc/rc.local"
+
+(
+    echo '#!/bin/bash'
+    echo 'echo "puppetize output is in puppetize.log"'
+    echo '/bin/bash /root/puppetize.sh 2>&1 | tee /root/puppetize.log'
+) > ${rc_local}~
+mv -f ${rc_local}~ ${rc_local} || fail
+chmod +x ${rc_local} || fail
+
+
+exit 0
