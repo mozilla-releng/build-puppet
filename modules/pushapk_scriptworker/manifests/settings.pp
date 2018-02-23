91980bdfde7d0e851d93b6362af5e23a63bba9ed
Johan Lorenzo <jlorenzo@mozilla.com>
2018-02-23 13:29:15 +0000

    Backed out changeset a5c22656b3c8
    This broke all signing tasks with this error:
       taskcluster.exceptions.TaskclusterRestFailure: Artifact expires (2019-02-23T12:29:56.220Z) after the task expiration 2019-02-23T09:59:28.158Z (task.expires < expires)

--- modules/pushapk_scriptworker/manifests/settings.pp
+++ modules/pushapk_scriptworker/manifests/settings.pp
@@ -48,8 +48,6 @@ class pushapk_scriptworker::settings {
     $worker_group                        = $_env_config['worker_group']
     $worker_type                         = $_env_config['worker_type']
 
-    $artifact_expiration_hours           = $_env_config['artifact_expiration_hours']
-
     $sign_chain_of_trust                 = $_env_config['sign_chain_of_trust']
     $verify_chain_of_trust               = $_env_config['verify_chain_of_trust']
     $verify_cot_signature                = $_env_config['verify_cot_signature']
