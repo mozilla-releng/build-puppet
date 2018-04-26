0d1f819f587c769f6d2170a783926c2ae36d119b
Dragos Crisan <dcrisan@mozilla.com>
2018-04-26 11:30:15 +0300

    Bug 1433532 - mig-agent fails to connect via proxy or direct connect, r=jwatkins, dhouse

--- modules/mig/manifests/agent/daemon.pp
+++ modules/mig/manifests/agent/daemon.pp
@@ -14,10 +14,32 @@ class mig::agent::daemon {
     # on package update, shutdown the old agent and start the new one
     # when the package is upgraded, exec a new instance of the agent
     $mig_path = $::operatingsystem ? { /(CentOS|Ubuntu)/ => '/sbin/mig-agent', Darwin => '/usr/local/bin/mig-agent' }
-    exec {
-        'restart mig':
-            command     => "${mig_path} -q=shutdown; ${mig_path}",
-            subscribe   => Class['packages::mozilla::mig_agent'],
-            refreshonly => true
+    case $::operatingsystem {
+        Ubuntu: {
+            case $::operatingsystemrelease {
+                16.04: {
+                    exec {
+                        'kill mig':
+                            command   => "/bin/kill -s 2 $(${mig_path} -q=pid); ${mig_path}",
+                            subscribe => Class['packages::mozilla::mig_agent'],
+                            notify    => Service['mig-agent']
+                    }
+                    service {
+                        'mig-agent':
+                            ensure   => running,
+                            enable   => true,
+                            provider => 'systemd'
+                    }
+                }
+            }
+        }
+        default: {
+            exec {
+                'restart mig':
+                    command     => "/bin/kill -s 2 $(${mig_path} -q=pid); ${mig_path}",
+                    subscribe   => Class['packages::mozilla::mig_agent'],
+                    refreshonly => true
+            }
+        }
     }
 }
