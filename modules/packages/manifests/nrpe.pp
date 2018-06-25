150b1a7ce246f0cb13ae202cc7cd7b6dcd41caee
Dragos Crisan <dcrisan@mozilla.com>
2018-06-25 12:45:35 +0300

    Bug 1458468 - Update nrpe package on roller servers

--- modules/packages/manifests/nrpe.pp
+++ modules/packages/manifests/nrpe.pp
@@ -2,15 +2,40 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class packages::nrpe {
+
+    anchor {
+        'packages::nrpe::begin': ;
+        'packages::nrpe::end': ;
+    }
+
     case $::operatingsystem {
         Ubuntu: {
-            package {
-                'nagios-nrpe-server':
-                    ensure          => latest,
-                    install_options => [ '--no-install-recommends' ];
-                'nagios-plugins':
-                    ensure          => latest,
-                    install_options => [ '--no-install-recommends' ];
+            case $::operatingsystemrelease {
+                12.04, 14.04: {
+                    package {
+                        'nagios-nrpe-server':
+                            ensure          => latest,
+                            install_options => [ '--no-install-recommends' ];
+                        'nagios-plugins':
+                            ensure          => latest,
+                            install_options => [ '--no-install-recommends' ];
+                    }
+                }
+                16.04: {
+                    realize(Packages::Aptrepo['nrpe'])
+                        Anchor['packages::nrpe::begin'] ->
+                        package {
+                            'nagios-nrpe-server':
+                                ensure          => '3.2.1-1ubuntu1_amd64',
+                                install_options => [ '--no-install-recommends' ];
+                            'nagios-nrpe-plugin':
+                                ensure          => '3.2.1-1ubuntu1_amd64',
+                                install_options => [ '--no-install-recommends' ];
+                        } -> Anchor['packages::nrpe::end']
+                }
+                default: {
+                    fail ("Cannot install on Ubuntu ${::operatingsystemrelease}")
+                }
             }
         }
         CentOS: {
--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -225,7 +225,7 @@ class packages::setup {
             }
             # to flush the package index, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 42
+            $repoflag = 43
             file {
                 '/etc/.repo-flag':
                     content =>
@@ -375,6 +375,11 @@ class packages::setup {
                     options      => ['arch=amd64'],
                     gpg_id       => '0EBFCD88',
                     gpg_key      => 'puppet:///modules/packages/0EBFCD88.txt';
+                'nrpe':
+                    url_path     => 'repos/apt/custom/nrpe',
+                    distribution => $::lsbdistcodename,
+                    options      => ['trusted=yes'],
+                    components   => ['all'];
             }
         }
         Darwin: {
