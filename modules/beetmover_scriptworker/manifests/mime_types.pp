021b2ba463a15d631cbba4efc183ef4396d93551
Johan Lorenzo <jlorenzo@mozilla.com>
2018-08-13 17:27:35 +0200

    Bug 1470942 - Add Maven-like buckets to beetmover (#87)
    * Add aar, pom, md5, and sha1 mime types
    * Set max file size to 100MB
    * Bump beetmoverscript to 7.8.0

---
+++ modules/beetmover_scriptworker/manifests/mime_types.pp
@@ -0,0 +1,21 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+class beetmover_scriptworker::mime_types {
+
+    case $::operatingsystem {
+        # This file is used by google-api-python-client to make sure it's pushing an APK. It relies on
+        # https://docs.python.org/3/library/mimetypes.html which needs this file, no matter what disto
+        # we're on. Without it, google-api-python-client refuses to handle files.
+        CentOS: {
+            file { '/etc/mime.types':
+                mode   => '0644',
+                source => 'puppet:///modules/beetmover_scriptworker/mime.types',
+            }
+        }
+        default: {
+            fail("cannot install on ${::operatingsystem}")
+        }
+    }
+}
