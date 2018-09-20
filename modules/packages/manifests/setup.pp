742cf251c66dd88b665719380ec7533c0bdf7d02
Dragos Crisan <dcrisan@mozilla.com>
2018-09-20 15:16:44 +0300

    Updated repo flag and change hg version for centos

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -190,7 +190,7 @@ class packages::setup {
 
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 95
+            $repoflag = 96
             file {
                 '/etc/.repo-flag':
                     content =>
