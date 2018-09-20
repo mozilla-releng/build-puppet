fea31d43789bc5f846abbedb60bb97b7a47f968b
Dragos Crisan <dcrisan@mozilla.com>
2018-09-20 10:42:10 +0300

    Updated repo flag

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -225,7 +225,7 @@ class packages::setup {
             }
             # to flush the package index, increase this value by one (or
             # anything, really, just change it).
-            $repoflag = 46
+            $repoflag = 47
             file {
                 '/etc/.repo-flag':
                     content =>
