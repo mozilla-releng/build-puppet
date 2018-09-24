777e630d560f4da0b27c850502f5b5f25146ef23
Ben Hearsum <bhearsum@mozilla.com>
2018-09-24 13:17:03 -0400

    Kill old modules (#225)
    * Remove dead machines from moco-nodes.pp.
    
    * Remove a ton of unused modules and toplevel manifests.

--- modules/aws_manager/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at http://mozilla.org/MPL/2.0/.
-class aws_manager {
-    include aws_manager::install
-    include aws_manager::secrets
-    include aws_manager::cron
-    include nrpe::check::check_stop_idle
-}
