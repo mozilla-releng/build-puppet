466fc926df477223cf75fe1faa749a0793c6338a
Dragos Crisan <dcrisan@mozilla.com>
2018-12-17 16:53:07 +0200

    Disable kvm modules

--- modules/disableservices/manifests/kvm.pp
+++ modules/disableservices/manifests/kvm.pp
@@ -2,12 +2,25 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 class disableservices::kvm {
+    include concat::setup
     case $::operatingsystem {
         Ubuntu: {
-            package {
-                ['qemu-kvm']:
-                    ensure => absent;
+          case $::operatingsystemrelease {
+            '16.04': {
+                concat {
+                    '/etc/modprobe.d/blacklist.conf':
+                        owner => root,
+                        group => root,
+                        mode  => filemode(0644);
+                }
+                # need at least one fragment, or concat will fail:
+                concat::fragment {
+                    'blacklist kvm':
+                        target  => '/etc/modprobe.d/blacklist.conf',
+                        content => "blacklist kvm\nblacklist kvm-intel\n"
+                }
             }
+          }
         }
     }
 }
