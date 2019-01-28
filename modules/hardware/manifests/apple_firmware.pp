fdc5d9b0e96be2643712dfa9b58056d8681ab4aa
Jake Watkins <jwatkins@mozilla.com>
2019-01-28 11:50:38 -0800

    Fix puppet lint warnings

--- modules/hardware/manifests/apple_firmware.pp
+++ modules/hardware/manifests/apple_firmware.pp
@@ -8,7 +8,7 @@ class hardware::apple_firmware {
         # Add if conditions for each hardware model explicitly supported
         # then add the list of acceptable firmware version for that supported model
         if $::sp_machine_model == 'Macmini7,1' and !($::sp_boot_rom_version in ['MM71.0232.B00']) {
-            fail("Boot ROM version $::sp_boot_rom_version does not match an acceptable version for hardware model $::sp_machine_model")
+            fail("Boot ROM version ${::sp_boot_rom_version} does not match an acceptable version for hardware model ${::sp_machine_model}")
         }
     }
 }
