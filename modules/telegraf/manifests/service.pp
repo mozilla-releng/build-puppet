71ac418ef073da1bf81027993a4592f18b91a729
Dave House <dhouse@mozilla.com>
2019-04-08 10:56:19 -0600

    update telegraf service control

--- modules/telegraf/manifests/service.pp
+++ modules/telegraf/manifests/service.pp
@@ -4,13 +4,27 @@
 #
 class telegraf::service {
 
+  case $::operatingsystem {
+    'Ubuntu' : {
+      $service_provider = 'systemd'
+    }
+    'Darwin' : {
+      $service_name = 'com.influxdb.telegraf'
+    }
+    default : {
+      $service_provider = undef
+    }
+  }
+
   if $::telegraf::manage_service {
     service { 'telegraf':
+      name      => $service_name,
       ensure    => $telegraf::service_ensure,
       hasstatus => $telegraf::service_hasstatus,
       enable    => $telegraf::service_enable,
       restart   => $telegraf::service_restart,
       require   => Class['::telegraf::config'],
+      provider  => $service_provider,
     }
   }
 }
