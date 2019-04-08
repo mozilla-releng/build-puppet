e45c1f8592deca3f3961fef245a593ee6b5eec1b
Dave House <dhouse@mozilla.com>
2019-04-08 10:56:50 -0600

    nit: see telegraf conf directory before file

--- modules/telegraf/manifests/config.pp
+++ modules/telegraf/manifests/config.pp
@@ -5,15 +5,6 @@
 class telegraf::config inherits telegraf {
 
   file {
-    $::telegraf::config_file:
-      ensure  => file,
-      content => template('telegraf/telegraf.conf.erb'),
-      owner   => $::telegraf::config_file_owner,
-      group   => $::telegraf::config_file_group,
-      mode    => '0640',
-      notify  => Class['::telegraf::service'],
-      require => Class['::telegraf::install'],
-    ;
     $::telegraf::config_folder:
       ensure  => directory,
       owner   => $::telegraf::config_file_owner,
@@ -21,6 +12,15 @@ class telegraf::config inherits telegraf {
       mode    => '0770',
       purge   => $::telegraf::purge_config_fragments,
       recurse => true,
+      notify  => Class['::telegraf::service'],
+      require => Class['::telegraf::install'],
+    ;
+    $::telegraf::config_file:
+      ensure  => file,
+      content => template('telegraf/telegraf.conf.erb'),
+      owner   => $::telegraf::config_file_owner,
+      group   => $::telegraf::config_file_group,
+      mode    => '0640',
       notify  => Class['::telegraf::service'],
       require => Class['::telegraf::install'],
     ;
