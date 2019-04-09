4cdbf36b651be4f30d85f3ff2366b7beae8ff120
Dragos Crisan <dcrisan@mozilla.com>
2019-04-09 11:34:08 +0300

    Bug 1500050 - Prevent restart nrpe service on every puppet run (#442)
    * Add conditions to run exec blocks, to prevent nrpe restart after each puppet run
    
    * Add conditions to run exec blocks, to prevent nrpe restart after each puppet run
    
    * Add conditions to run exec blocks, to prevent nrpe restart after each puppet run
    
    * Add conditions to run exec blocks, to prevent nrpe restart after each puppet run
    
    * Revert changes for ubuntu file section

--- modules/nrpe/manifests/base.pp
+++ modules/nrpe/manifests/base.pp
@@ -51,7 +51,8 @@ class nrpe::base {
             if $::operatingsystem == 'CentOS' {
                 exec { 'change pid file path':
                     command => "sed -i 's/PID_FILE=\/var\/run\/nrpe\/nrpe.pid/PID_FILE=\/var\/run\/nrpe.pid/g' /etc/init.d/nrpe",
-                    path    => ['/bin', '/sbin'],
+                    path    => ['/bin', '/sbin', '/usr/bin'],
+                    unless  => 'test `grep -w "PID_FILE=\/var\/run\/nrpe.pid" /etc/init.d/nrpe`',
                     notify  => Class['nrpe::service'],
                     require => Class['packages::nrpe'];
                 }
