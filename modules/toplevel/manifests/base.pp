b8e45ad08dfec52f008024fba39a9dd3f05db180
davehouse <dhouse@mozilla.com>
2019-08-16 13:39:17 -0600

    disable collect service

--- modules/toplevel/manifests/base.pp
+++ modules/toplevel/manifests/base.pp
@@ -41,7 +41,7 @@ class toplevel::base {
         include packages::screen
         include users::global
         include powermanagement
-        include collectd
+        include collectd:disable
 
         # openssl ends up getting pulled in as a dependency everywhere, and we
         # want to carefully control its version, so include it everywhere.
