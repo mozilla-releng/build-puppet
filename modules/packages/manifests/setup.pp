470948cce415e87785a41c1370b1089e099deab2
Jake Watkins <jwatkins@mozilla.com>
2020-03-09 12:23:33 -0700

    Bug 1621107: upgrade bacula clients

--- modules/packages/manifests/setup.pp
+++ modules/packages/manifests/setup.pp
@@ -198,7 +198,7 @@ class packages::setup {
             # to flush the metadata cache, increase this value by one (or
             # anything, really, just change it).
 
-            $repoflag = 113
+            $repoflag = 114
 
             file {
                 '/etc/.repo-flag':
