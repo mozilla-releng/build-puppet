4ff95315c132be1a218ca0dc83463506494354a1
Jake Watkins <jwatkins@mozilla.com>
2020-06-19 15:04:13 -0700

    Clean up moco nodes

--- manifests/moco-nodes.pp
+++ manifests/moco-nodes.pp
@@ -2,17 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-## TaskCluster workers
-
-# OS X in mdc1 and mdc2 running generic worker
-node /^t-yosemite-r7-\d+\.test\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
-    $aspects          = [ 'low-security' ]
-    $slave_trustlevel = 'try'
-    $worker_type = "gecko-t-osx-1010"
-    include fw::profiles::osx_taskcluster_worker
-    include toplevel::worker::releng::generic_worker::test::gpu
-}
-
 # Linux on moonshot in mdc1 running generic worker
 # ms == moonshot == https://www.hpe.com/emea_europe/en/servers/moonshot.html
 # xe == xen virtual machines on moonshot
@@ -44,20 +33,9 @@ node /^t-linux64-(ms|xe)-\d{3}\.test\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
     include toplevel::worker::releng::generic_worker::test::gpu
 }
 
-## signing servers
-
-node /^mac-(v2-|)signing\d+\.srv\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
-    # mac signing servers
-    $aspects       = [ 'maximum-security' ]
-    $timezone      = 'GMT'
-    $only_user_ssh = true
-    include fw::profiles::mac_signing
-    include toplevel::server::signing
-}
-
 ## puppetmasters
 
-node /^releng-puppet\d+\.srv\.releng\.(mdc1|mdc2|use1|usw2)\.mozilla\.com$/ {
+node /^releng-puppet\d+\.srv\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
     $aspects       = [ 'maximum-security' ]
     $only_user_ssh = true
     include fw::profiles::puppetmasters
@@ -142,13 +120,6 @@ node /^partner-repack-\d+\.srv\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
     include toplevel::server
 }
 
-# mergeday
-
-node /^mergeday\d+\.srv\.releng\.(use1|usw2)\.mozilla\.com$/ {
-    $aspects = [ 'high-security' ]
-    include toplevel::server::mergeday
-}
-
 # Relops Controller
 
 node /^roller1\.srv\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
@@ -164,54 +135,14 @@ node /^roller-dev1\.srv\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
     include toplevel::server::roller
 }
 
-## buildbot masters
+## Log Aggregators
 
-node 'buildbot-master01.bb.releng.use1.mozilla.com' {
-    $aspects       = [ 'high-security' ]
-    $only_user_ssh = true
-    include toplevel::server::buildmaster
-    include toplevel::mixin::bouncer_check
-    include toplevel::mixin::signing_server_cert_check
-}
-
-node 'buildbot-master77.bb.releng.use1.mozilla.com' {
-    $aspects         = [ 'high-security' ]
-    $only_user_ssh   = true
-    include toplevel::server::buildmaster
-    include toplevel::mixin::signing_server_cert_check
-}
-
-node /^log-aggregator\d+\.srv\.releng\.(mdc1|mdc2|use1|usw2)\.mozilla\.com$/ {
+node /^log-aggregator\d+\.srv\.releng\.(mdc1|mdc2)\.mozilla\.com$/ {
     $aspects                = [ 'high-security' ]
     $is_log_aggregator_host = 'true'
     include fw::profiles::log_aggregator
     include toplevel::server::log_aggregator
 }
-
-# Workers in osx level 3 trusted pool
-node 't-yosemite-r7-471.test.releng.mdc1.mozilla.com',
-    't-yosemite-r7-472.test.releng.mdc1.mozilla.com',
-    't-yosemite-r7-235.test.releng.mdc2.mozilla.com',
-    't-yosemite-r7-236.test.releng.mdc2.mozilla.com' {
-    $aspects          = [ 'maximum-security' ]
-    $slave_trustlevel = 'core'
-    $worker_type = "gecko-3-t-osx-1010"
-    include fw::profiles::osx_taskcluster_worker
-    include toplevel::worker::releng::generic_worker::test::gpu
-}
-
-# Workers in osx staging pool
-node 't-yosemite-r7-380.test.releng.mdc1.mozilla.com',
-    't-yosemite-r7-394.test.releng.mdc1.mozilla.com',
-    't-yosemite-r7-100.test.releng.mdc2.mozilla.com',
-    't-yosemite-r7-101.test.releng.mdc2.mozilla.com' {
-    $aspects          = [ 'low-security', 'staging' ]
-    $slave_trustlevel = 'try'
-    $worker_type = "gecko-t-osx-1010-beta"
-    include fw::profiles::osx_taskcluster_worker
-    include toplevel::worker::releng::generic_worker::test::gpu
-}
-
 
 # Workers in linux talos staging pool
 node 't-linux64-ms-280.test.releng.mdc1.mozilla.com',
